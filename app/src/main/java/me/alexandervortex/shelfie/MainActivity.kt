e71b1a80e2fb8c9d8c1a38880b95140e2f1f9646
Alexander Vortex <avorteks@alfabank.ru>
2025-09-06 14:07:54 +0200

    fix gitignore

--- app/src/main/java/me/alexandervortex/shelfie/MainActivity.kt
+++ app/src/main/java/me/alexandervortex/shelfie/MainActivity.kt
@@ -14,13 +14,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import me.alexandervortex.shelfie.ui.theme.ShelfieTheme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ShelfieTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
