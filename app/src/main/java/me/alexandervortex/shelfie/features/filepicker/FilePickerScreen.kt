2a1c7052b1b03ca056f254b204a888b9843c2ef7
Alexander Vortex <avorteks@alfabank.ru>
2025-09-06 18:03:41 +0200

    RDR-97 filepicker screen

--- app/src/main/java/me/alexandervortex/shelfie/features/filepicker/FilePickerScreen.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/filepicker/FilePickerScreen.kt
@@ -1,10 +1,39 @@
 package me.alexandervortex.shelfie.features.filepicker
 
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 
 @Composable
-fun FilePickerScreen(navController: NavHostController, viewModel: FilePickerViewModel) {
-    Text("This is filepicker screen")
+fun FilePickerScreen(
+    navController: NavHostController,
+    viewModel: FilePickerViewModel,
+) {
+    val context = LocalContext.current
+    val picker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.OpenDocument()
+    ) { uri: Uri? ->
+        uri ?: return@rememberLauncherForActivityResult
+
+        try {
+            context.contentResolver.takePersistableUriPermission(
+                uri,
+                Intent.FLAG_GRANT_READ_URI_PERMISSION
+            )
+        } catch (e: SecurityException) {
+            /* провайдер мог не дать persist */
+        }
+
+//        TODO: viewModel.onPicked(uri)
+    }
+
+    Button({
+        picker.launch(arrayOf("text/*", "application/*", "application/octet-stream"))
+    }) { Text("Выбрать FB2 книгу") }
 }
