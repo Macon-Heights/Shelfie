e1809c690201658f91706d66148d3a1772b99c8a
Alexander Vortex <avorteks@alfabank.ru>
2025-09-06 15:32:48 +0200

    RDR-90 viewer screen

--- app/src/main/java/me/alexandervortex/shelfie/MainViewModel.kt
+++ app/src/main/java/me/alexandervortex/shelfie/MainViewModel.kt
@@ -1,9 +1,33 @@
 package me.alexandervortex.shelfie
 
+import android.content.Context
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.kursx.parser.fb2.FictionBook
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.io.File
+import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel : ViewModel() {
+class MainViewModel
+@Inject constructor() : ViewModel() {
 
+    val error = mutableStateOf("no error")
+    val bookSample: MutableState<FictionBook?> = mutableStateOf(null)
+
+    fun initScreenData(context: Context) {
+        try {
+            val inputStream = context.assets.open("sample.fb2")
+            val bookFile = File(context.cacheDir, "sample.fb2")
+            inputStream.use { input ->
+                bookFile.outputStream().use {
+                    input.copyTo(it)
+                }
+            }
+            bookSample.value = FictionBook(bookFile)
+        } catch (e: Exception) {
+            error.value = e.localizedMessage ?: "unknown error"
+        }
+    }
 }
---
+++ app/src/main/java/me/alexandervortex/shelfie/ViewerScreen.kt
@@ -0,0 +1,30 @@
+package me.alexandervortex.shelfie
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import com.kursx.parser.fb2.Element
+import com.kursx.parser.fb2.Section
+
+@Composable
+fun ViewerScreen(viewModel: MainViewModel) {
+    val context = LocalContext.current
+    LaunchedEffect(true) { viewModel.initScreenData(context) }
+
+    val items: List<String> = viewModel.bookSample.value?.let { book ->
+        book.body.sections.flatMap { section: Section ->
+            section.elements.map { element: Element? ->
+                element?.text
+            }
+        }
+    }?.filterNotNull().orEmpty()
+
+    LazyColumn {
+        items(items) { name: String ->
+            Text(text = name)
+        }
+    }
+}
