1c670d614bb2d62986bbc6ce63d94028e1f815d3
Alexander Vortex <avorteks@alfabank.ru>
2025-09-13 15:57:27 +0200

    RDR-99 repo work now

--- app/src/main/java/me/alexandervortex/shelfie/features/filepicker/FilePickerViewModel.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/filepicker/FilePickerViewModel.kt
@@ -1,11 +1,45 @@
 package me.alexandervortex.shelfie.features.filepicker
 
+import android.net.Uri
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import me.alexandervortex.shelfie.data.db.entiry.BookEntity
+import me.alexandervortex.shelfie.data.repository.BookRepo
 import javax.inject.Inject
 
 @HiltViewModel
 class FilePickerViewModel
-@Inject constructor() : ViewModel() {
+@Inject constructor(
+    private val repo: BookRepo,
+) : ViewModel() {
 
+    val books = mutableStateListOf<BookEntity>()
+
+    fun onBookAdded(uri: Uri) {
+        viewModelScope.launch {
+            val book = BookEntity(
+                uid = uri.hashCode(),
+                uri = uri.toString(),
+                name = uri.lastPathSegment ?: "noname"
+            )
+            repo.insert(book)
+            loadBooks()
+        }
+    }
+
+    fun loadBooks() {
+        viewModelScope.launch {
+            val result = repo.getAll()
+            books.clear()
+            books.addAll(result.toMutableStateList())
+        }
+    }
+
+    fun setCurrentBook(item: BookEntity) {
+        repo.currentBook = item
+    }
 }
