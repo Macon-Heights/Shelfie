e59ff365c4f60677a01de6faf6c3d0adc86a28b2
Alexander Vortex <avorteks@alfabank.ru>
2025-09-13 12:24:10 +0200

    RDR-99 add db, dao, entity

---
+++ app/src/main/java/me/alexandervortex/shelfie/data/db/BookDb.kt
@@ -0,0 +1,12 @@
+package me.alexandervortex.shelfie.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import me.alexandervortex.shelfie.data.db.dao.BookDao
+import me.alexandervortex.shelfie.data.db.entiry.BookEntity
+
+@Database(entities = [BookEntity::class], version = 1)
+abstract class BookDb : RoomDatabase() {
+
+    abstract fun bookDao(): BookDao
+}
