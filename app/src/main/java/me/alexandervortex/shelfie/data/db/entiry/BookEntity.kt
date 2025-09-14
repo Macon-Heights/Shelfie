7e6da6e870e7cd0ece44f654d2e5215b1deebaf7
Alexander Vortex <avorteks@alfabank.ru>
2025-09-14 11:25:33 +0200

    RDR-101 add fields to book entity

--- app/src/main/java/me/alexandervortex/shelfie/data/db/entiry/BookEntity.kt
+++ app/src/main/java/me/alexandervortex/shelfie/data/db/entiry/BookEntity.kt
@@ -7,7 +7,27 @@ import me.alexandervortex.shelfie.data.db.dao.BOOK_TABLE
 
 @Entity(tableName = BOOK_TABLE)
 data class BookEntity(
-    @PrimaryKey val uid: Int,
-    @ColumnInfo(name = "uri") val uri: String,
-    @ColumnInfo(name = "name") val name: String,
+    @PrimaryKey
+    val id: Int,  // contentId (SHA-256 текста/канон. XML)
+
+    @ColumnInfo(name = "fb2DocumentId")
+    val fb2DocumentId: String?, // <document-info><id>
+
+    @ColumnInfo(name = "title")
+    val title: String,
+
+    @ColumnInfo(name = "author")
+    val author: String,
+
+    @ColumnInfo(name = "lang")
+    val lang: String,
+
+    @ColumnInfo(name = "version")
+    val version: Int,
+
+    @ColumnInfo(name = "uri")
+    val uri: String,
+
+    @ColumnInfo(name = "addedAt")
+    val addedAt: Long,
 )
