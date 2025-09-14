6ca36c81b4dfad1df71d3554aef38fb54598949d
Alexander Vortex <avorteks@alfabank.ru>
2025-09-14 17:54:22 +0200

    RDR-102 return db storage

--- app/src/main/java/me/alexandervortex/shelfie/data/db/entiry/BookEntity.kt
+++ app/src/main/java/me/alexandervortex/shelfie/data/db/entiry/BookEntity.kt
@@ -8,14 +8,20 @@ import me.alexandervortex.shelfie.data.db.dao.BOOK_TABLE
 @Entity(tableName = BOOK_TABLE)
 data class BookEntity(
     @PrimaryKey
-    val id: Int,  // contentId (SHA-256 текста/канон. XML)
+    val id: Int,
 
     @ColumnInfo(name = "fb2DocumentId")
-    val fb2DocumentId: String?, // <document-info><id>
+    val fb2DocumentId: String?,
+
+    @ColumnInfo(name = "uri")
+    val uri: String,
 
     @ColumnInfo(name = "title")
     val title: String,
 
-    @ColumnInfo(name = "uri")
-    val uri: String,
+    @ColumnInfo(name = "author")
+    val author: String?,
+
+    @ColumnInfo(name = "image")
+    val image: ByteArray?,
 )
