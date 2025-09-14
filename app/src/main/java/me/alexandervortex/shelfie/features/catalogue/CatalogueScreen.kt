aa7e4d10957544f2c7d7b72de35209c76bc35271
Alexander Vortex <avorteks@alfabank.ru>
2025-09-14 16:49:05 +0200

    RDR-102 make big title

--- app/src/main/java/me/alexandervortex/shelfie/features/catalogue/CatalogueScreen.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/catalogue/CatalogueScreen.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -12,6 +13,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @Composable
@@ -38,9 +42,14 @@ fun CatalogueScreen(
     }
     // endregion
 
-    LazyColumn {
+    LazyColumn(modifier = Modifier.padding(32.dp)) {
         item {
-            Text("Твои книги")
+            Text(
+                text = "Your\nBooks",
+                lineHeight = 72.sp,
+                fontSize = 72.sp,
+                fontWeight = FontWeight.Black,
+            )
         }
         items(vm.books) { item ->
             Text(item.title, Modifier.clickable {
