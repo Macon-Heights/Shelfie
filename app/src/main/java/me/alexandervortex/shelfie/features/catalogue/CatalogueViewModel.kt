e2beb8644c95cbf01e83f2aba7c4973893793f00
Alexander Vortex <avorteks@alfabank.ru>
2025-09-14 14:51:03 +0200

    RDR-101 add catalogue screen

---
+++ app/src/main/java/me/alexandervortex/shelfie/features/catalogue/CatalogueViewModel.kt
@@ -0,0 +1,11 @@
+package me.alexandervortex.shelfie.features.catalogue
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CatalogueViewModel
+@Inject constructor() : ViewModel() {
+
+}
