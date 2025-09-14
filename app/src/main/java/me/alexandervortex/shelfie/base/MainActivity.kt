2728ad792efe8c3373e367b89162a0f3f3147da0
Alexander Vortex <avorteks@alfabank.ru>
2025-09-14 16:49:05 +0200

    RDR-102 make big title

--- app/src/main/java/me/alexandervortex/shelfie/base/MainActivity.kt
+++ app/src/main/java/me/alexandervortex/shelfie/base/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             ShelfieTheme {
                 RouterScreen()
