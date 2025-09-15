44f787c4684352660c4368efe48a4c1a3610564d
Alexander Vortex <avorteks@alfabank.ru>
2025-09-15 20:14:16 +0200

    RDR-105 add base catalogue vm

--- app/src/main/java/me/alexandervortex/shelfie/features/catalogue/CatalogueViewModel.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/catalogue/CatalogueViewModel.kt
@@ -3,10 +3,10 @@ package me.alexandervortex.shelfie.features.catalogue
 import android.content.Context
 import android.net.Uri
 import androidx.compose.runtime.mutableStateListOf
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import me.alexandervortex.shelfie.base.BaseCatalogueViewModel
 import me.alexandervortex.shelfie.base.ext.toBook
 import me.alexandervortex.shelfie.data.db.entiry.BookEntity
 import me.alexandervortex.shelfie.data.repository.RepositoryImpl
@@ -16,11 +16,11 @@ import javax.inject.Inject
 class CatalogueViewModel
 @Inject constructor(
     private val repository: RepositoryImpl,
-) : ViewModel() {
+) : BaseCatalogueViewModel() {
 
-    val books = mutableStateListOf<BookEntity>()
+    override val books = mutableStateListOf<BookEntity>()
 
-    fun addBookByUri(uri: Uri, context: Context) {
+    override fun addBookByUri(uri: Uri, context: Context) {
         viewModelScope.launch {
             val book = uri.toBook(context)
             repository.addBook(book, uri)
@@ -30,14 +30,14 @@ class CatalogueViewModel
         // и обновляю список текущих
     }
 
-    fun loadBooks() {
+    override fun loadBooks() {
         viewModelScope.launch {
             books.clear()
             books.addAll(repository.getBooks())
         }
     }
 
-    fun setCurrentBook(item: BookEntity) {
+    override fun setCurrentBook(item: BookEntity) {
         repository.currentBook = item
     }
 }
