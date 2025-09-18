81a4dd77e29451823727ccccfa5e290dacd454d2
Alexander Vortex <avorteks@alfabank.ru>
2025-09-18 18:23:30 +0200

    RDR-105 create title component

---
+++ app/src/main/java/me/alexandervortex/shelfie/ui/component/TitleComponent.kt
@@ -0,0 +1,36 @@
+package me.alexandervortex.shelfie.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+private const val size = 72
+private const val spacing = 16
+
+@Composable
+fun TitleComponent(
+    text: String?,
+    modifier: Modifier = Modifier,
+) {
+    text?.let {
+        Text(
+            text = text,
+            modifier = modifier.background(Color.Yellow),
+            fontSize = size.sp,
+            letterSpacing = spacing.sp,
+            fontWeight = FontWeight.Thin,
+        )
+    }
+
+}
+
+@Composable
+@Preview
+fun TitleComponent() {
+    TitleComponent("Your\nBooks")
+}
