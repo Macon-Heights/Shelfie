aedb08061785c615e0f117cc22e6c1e7f23583ed
Alexander Vortex <avorteks@alfabank.ru>
2025-09-27 16:25:37 +0200

    RDR-121 start of some refactoring (broken files)

--- app/src/main/java/me/alexandervortex/shelfie/data/mapper/BookModelMapper.kt
+++ app/src/main/java/me/alexandervortex/shelfie/data/mapper/BookModelMapper.kt
@@ -1,14 +1,14 @@
 package me.alexandervortex.shelfie.data.mapper
 
 import com.kursx.parser.fb2.FictionBook
-import com.kursx.parser.fb2.Section
 import me.alexandervortex.shelfie.data.model.BookModel
-import me.alexandervortex.shelfie.data.model.SectionModel
 import java.io.File
 import javax.inject.Inject
 
 class BookModelMapper
-@Inject constructor() {
+@Inject constructor(
+    private val sectionMapper: SectionModelMapper,
+) {
 
     fun map(
         file: File,
@@ -16,6 +16,7 @@ class BookModelMapper
     ): BookModel {
         val fictionBook = FictionBook(file)
         val localPath = file.absolutePath
+
         val title = fictionBook.description.titleInfo.bookTitle
         val author = fictionBook.description.titleInfo.authors.firstOrNull()?.fullName
         val year = fictionBook.description.publishInfo.year
@@ -28,16 +29,8 @@ class BookModelMapper
             author = author,
             year = year,
             sections = sections.map { section ->
-                mapSection(section)
+                sectionMapper.map(section)
             },
-        )
-    }
-
-    private fun mapSection(section: Section): SectionModel {
-        return SectionModel(
-            section.elements.map {
-                "${it.text.replace("\n", " ")}\n\n\n"
-            }
         )
     }
 }
---
+++ app/src/main/java/me/alexandervortex/shelfie/data/mapper/SectionModelMapper.kt
@@ -0,0 +1,17 @@
+package me.alexandervortex.shelfie.data.mapper
+
+import com.kursx.parser.fb2.Section
+import me.alexandervortex.shelfie.data.model.SectionModel
+import javax.inject.Inject
+
+class SectionModelMapper
+@Inject constructor() {
+
+    fun map(section: Section): SectionModel {
+        return SectionModel(
+            elements = section.elements.map {
+                "${it.text.replace("\n", " ")}\n\n\n"
+            }
+        )
+    }
+}
