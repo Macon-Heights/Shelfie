9b023209f50b05dbc7d7975bae01a3cfb65fe47c
Alexander Vortex <avorteks@alfabank.ru>
2025-09-28 19:06:51 +0200

    RDR-121 refactor my old shitty parser

--- app/src/main/java/me/alexandervortex/shelfie/data/mapper/FictionBookParser.kt
+++ app/src/main/java/me/alexandervortex/shelfie/data/mapper/FictionBookParser.kt
@@ -1,8 +1,8 @@
 package me.alexandervortex.shelfie.data.mapper
 
 import com.kursx.parser.fb2.FictionBook
+import me.alexandervortex.shelfie.data.model.BookFile
 import me.alexandervortex.shelfie.data.model.BookModel
-import java.io.File
 import javax.inject.Inject
 
 @Deprecated("GAVNO")
@@ -12,11 +12,11 @@ class FictionBookParser
 ) {
 
     fun parse(
-        file: File,
+        file: BookFile,
         id: String,
     ): BookModel {
-        val fictionBook = FictionBook(file)
-        val localPath = file.absolutePath
+        val fictionBook = FictionBook(file.file)
+        val localPath = file.path
 
         val title = fictionBook.description.titleInfo.bookTitle
         val author = fictionBook.description.titleInfo.authors.firstOrNull()?.fullName
