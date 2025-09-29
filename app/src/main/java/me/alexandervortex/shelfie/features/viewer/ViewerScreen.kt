23417f20b0552d21bf3d5ea19bef123c0cf2873b
Alexander Vortex <avorteks@alfabank.ru>
2025-09-30 00:58:53 +0200

    RDR-124 add mock components

--- app/src/main/java/me/alexandervortex/shelfie/features/viewer/ViewerScreen.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/viewer/ViewerScreen.kt
@@ -2,21 +2,17 @@ package me.alexandervortex.shelfie.features.viewer
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import me.alexandervortex.shelfie.ui.component.viewerui.BlockComponent
 import me.alexandervortex.shelfie.ui.model.SectionUi
 
 @Composable
@@ -36,38 +32,11 @@ fun ViewerScreen(
 
         items(sections) { section ->
             section.blocks.forEachIndexed { i, element ->
-                Text(
-                    textAlign = TextAlign.Justify,
-                    color = MaterialTheme.colorScheme.background,
-                    modifier = Modifier.background(randomColor(i)),
-                    text = element.toString(),
-                )
+                BlockComponent(element)
             }
-            Spacer(
-                Modifier
-                    .size(height = 24.dp, width = 240.dp)
-                    .background(Color.Red)
-            )
         }
     }
 
-}
-
-fun getColors(): List<Color> {
-    return listOf(
-        Color.Red,
-        Color.Green,
-        Color.Blue,
-        Color.Yellow,
-        Color.Magenta,
-        Color.Cyan,
-        Color.Gray,
-    )
-}
-
-inline fun randomColor(i: Int): Color {
-    val index = i % getColors().size
-    return getColors()[index]
 }
 
 @Composable
---
+++ app/src/main/java/me/alexandervortex/shelfie/ui/component/viewerui/ParagraphComponent.kt
@@ -0,0 +1,14 @@
+package me.alexandervortex.shelfie.ui.component.viewerui
+
+import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import me.alexandervortex.shelfie.ui.model.BlockUi
+
+@Composable
+fun ParagraphComponent(model: BlockUi.Paragraph) {
+    Text(model.text, color = MaterialTheme.colorScheme.onBackground)
+}
