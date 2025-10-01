640746f9d5cdbf7bb7edd579b16109824b63027f
Alexander Vortex <avorteks@alfabank.ru>
2025-10-02 01:07:38 +0200

    RDR-124 keep mapping

---
+++ app/src/main/java/me/alexandervortex/shelfie/ui/model/ElementUI.kt
@@ -0,0 +1,19 @@
+package me.alexandervortex.shelfie.ui.model
+
+sealed interface ElementUI {
+
+    data class ContainerElementUI(
+        val type: String?,
+        val elements: List<ElementUI>,
+    )
+
+    data class TextUI(
+        val text: String,
+    ) : ElementUI
+
+    data class ImageUI(
+        val image: ByteArray,
+    ) : ElementUI
+
+    data object EmptyLine
+}
