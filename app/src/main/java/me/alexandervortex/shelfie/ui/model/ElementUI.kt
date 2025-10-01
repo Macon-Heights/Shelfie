dd465a12151c39da91f82bda8ff49b2a28915981
Alexander Vortex <avorteks@alfabank.ru>
2025-10-01 23:54:34 +0200

    RDR-124 some refactorings

--- app/src/main/java/me/alexandervortex/shelfie/ui/model/ElementUI.kt
+++ app/src/main/java/me/alexandervortex/shelfie/ui/model/ElementUI.kt
@@ -1,5 +1,11 @@
 package me.alexandervortex.shelfie.ui.model
 
 sealed interface ElementUI {
-
+    data class TextUI(val text: String) : ElementUI
+    data class ImageUI(val image: ByteArray?) : ElementUI
+    data class EmptyLineUI(val dummy: Boolean = true) : ElementUI
+    data class StackElementUI(
+        val type: String,
+        val children: List<ElementUI>
+    ) : ElementUI
 }
