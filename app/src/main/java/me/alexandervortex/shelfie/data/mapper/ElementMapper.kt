0f12c0f25bd5d35ceb45bc49cdf48787e6d6798e
Alexander Vortex <avorteks@alfabank.ru>
2025-10-01 23:21:55 +0200

    RDR-124 add element mapper

---
+++ app/src/main/java/me/alexandervortex/shelfie/data/mapper/ElementMapper.kt
@@ -0,0 +1,12 @@
+package me.alexandervortex.shelfie.data.mapper
+
+import me.alexandervortex.shelfie.ui.model.ElementUI
+import javax.inject.Inject
+
+class ElementMapper
+@Inject constructor() {
+
+    fun map(): List<ElementUI> {
+        return emptyList()
+    }
+}
