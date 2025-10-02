59d61333c11bf391fd01621c45b3d13b35c64b3d
Alexander Vortex <avorteks@alfabank.ru>
2025-10-02 19:15:41 +0200

    RDR-126 little refactoring

---
+++ app/src/main/java/me/alexandervortex/shelfie/base/Document.kt
@@ -0,0 +1,27 @@
+package me.alexandervortex.shelfie.base
+
+import android.util.Base64
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+
+private const val BODY = "body"
+private const val TITLE_INFO = "description > title-info"
+private const val BINARY = "binary"
+private const val ID = "id"
+
+fun Document.getBody(): Element? {
+    return selectFirst(BODY)
+}
+
+fun Document.getTitleInfo(): Element? {
+    return selectFirst(TITLE_INFO)
+}
+
+fun Document.getBinaries(): Map<String, ByteArray> {
+    return select(BINARY)
+        .associate {
+            val binaryId = it.attr(ID)
+            val base64 = it.text().trim()
+            binaryId to Base64.decode(base64, Base64.DEFAULT)
+        }
+}
