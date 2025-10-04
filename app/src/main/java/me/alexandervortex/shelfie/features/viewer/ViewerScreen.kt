4479fe98b25cd4535b85b06284ed62562ef7d4f5
Alexander Vortex <avorteks@alfabank.ru>
2025-10-04 22:44:10 +0200

    RDR-131 commit probably bad code

--- app/src/main/java/me/alexandervortex/shelfie/features/viewer/ViewerScreen.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/viewer/ViewerScreen.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -90,8 +90,10 @@ fun ViewerScreen(
             modifier = Modifier.fillMaxSize()
         ) {
             val sections: List<ElementUI> = book?.elements.orEmpty()
-            items(sections) { section ->
-                ComponentUI(section)
+            itemsIndexed(sections) { index, section ->
+                ComponentUI(
+                    section, index == viewModel.CURRENT_INDEX.value
+                )
             }
         }
         // показывалка ошибки
