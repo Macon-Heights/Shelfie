c044340a5338c0abc57a20f6c7992727967a985b
Alexander Vortex <avorteks@alfabank.ru>
2025-10-04 18:20:04 +0200

    RDR-131 adapt colors

--- app/src/main/java/me/alexandervortex/shelfie/base/MainActivity.kt
+++ app/src/main/java/me/alexandervortex/shelfie/base/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import com.example.compose.ShelfieTheme
 import dagger.hilt.android.AndroidEntryPoint
 import me.alexandervortex.shelfie.features.navigate.RouterScreen
+import me.alexandervortex.shelfie.ui.theme.getColors
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -16,7 +20,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ShelfieTheme {
-                RouterScreen()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = getColors().background
+                ) { RouterScreen() }
             }
         }
     }
