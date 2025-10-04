0f783b0256b86619a573684512d0acc2cb539381
Alexander Vortex <avorteks@alfabank.ru>
2025-10-04 18:38:00 +0200

    RDR-131 theme repackaged

--- app/src/main/java/me/alexandervortex/shelfie/base/MainActivity.kt
+++ app/src/main/java/me/alexandervortex/shelfie/base/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.compose.ShelfieTheme
+import me.alexandervortex.shelfie.ui.theme.ShelfieTheme
 import dagger.hilt.android.AndroidEntryPoint
 import me.alexandervortex.shelfie.features.navigate.RouterScreen
 import me.alexandervortex.shelfie.ui.theme.getColors
