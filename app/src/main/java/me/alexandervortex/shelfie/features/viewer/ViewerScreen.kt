d6ba4a9219a3cd977c1b34a66f6365448c2f74cf
Alexander Vortex <avorteks@alfabank.ru>
2025-10-04 04:14:43 +0200

    RDR-129 add tts and its worked

--- app/src/main/java/me/alexandervortex/shelfie/features/viewer/ViewerScreen.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/viewer/ViewerScreen.kt
@@ -1,16 +1,24 @@
 package me.alexandervortex.shelfie.features.viewer
 
+import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
@@ -55,17 +63,40 @@ fun ViewerScreen(
         onDispose { lifecycleOwner.lifecycle.removeObserver(observer) }
     }
 
-    LazyColumn(
-        state = listState,
-        contentPadding = PaddingValues(32.dp),
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-    ) {
-        val sections: List<ElementUI> = book?.elements.orEmpty()
-        items(sections) { section ->
-            ComponentUI(section)
+    Box {
+        LazyColumn(
+            state = listState,
+            contentPadding = PaddingValues(32.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            val sections: List<ElementUI> = book?.elements.orEmpty()
+            items(sections) { section ->
+                ComponentUI(section)
+            }
+        }
+        if (viewModel.error.value.isNotBlank()) {
+            Toast.makeText(LocalContext.current, viewModel.error.value, Toast.LENGTH_SHORT).show()
+        }
+        Button(
+            colors = ButtonColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                disabledContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                disabledContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            ),
+            modifier = Modifier
+                .padding(16.dp)
+                .size(64.dp),
+            onClick = {
+                viewModel.togglePlayPause()
+            }
+        ) {
+            Icon(
+                imageVector = viewModel.buttonIcon.value,
+                "",
+            )
         }
     }
-
 }
