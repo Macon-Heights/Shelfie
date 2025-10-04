aae2fbc376f99925cde3ed0df66aa67ee4e963bb
Alexander Vortex <avorteks@alfabank.ru>
2025-10-04 13:47:00 +0200

    RDR-129 add icons

---
+++ app/src/main/java/me/alexandervortex/shelfie/ui/theme/Icons.kt
@@ -0,0 +1,11 @@
+package me.alexandervortex.shelfie.ui.theme
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+
+private val icons = Icons.Default
+
+// Player
+val IC_PLAY = icons.PlayArrow
+val IC_PAUSE = icons.Pause
