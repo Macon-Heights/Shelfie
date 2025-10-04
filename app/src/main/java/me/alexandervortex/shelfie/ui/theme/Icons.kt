8f560f2ea294f0a29ff13d00195f721c6f21dedf
Alexander Vortex <avorteks@alfabank.ru>
2025-10-04 14:18:08 +0200

    RDR-129 start big tts refactoring (just replace icons for now)

--- app/src/main/java/me/alexandervortex/shelfie/ui/theme/Icons.kt
+++ app/src/main/java/me/alexandervortex/shelfie/ui/theme/Icons.kt
@@ -1,11 +1,16 @@
 package me.alexandervortex.shelfie.ui.theme
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
 
 private val icons = Icons.Default
 
+// Catalogue
+val IC_ADD = icons.Add
+
 // Player
 val IC_PLAY = icons.PlayArrow
 val IC_PAUSE = icons.Pause
+
