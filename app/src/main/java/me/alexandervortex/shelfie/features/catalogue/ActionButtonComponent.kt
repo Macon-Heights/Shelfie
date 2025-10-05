f0c774ef20339fb28d0f4ce9259e837fe0d9e866
Alexander Vortex <avorteks@alfabank.ru>
2025-10-05 17:26:58 +0200

    RDR-133 move and rename action button

--- app/src/main/java/me/alexandervortex/shelfie/features/catalogue/ActionButtonComponent.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/catalogue/ActionButtonComponent.kt
@@ -1,4 +1,4 @@
-package me.alexandervortex.shelfie.ui.component
+package me.alexandervortex.shelfie.features.catalogue
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -11,28 +11,29 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import me.alexandervortex.shelfie.ui.theme.getColors
 
+private const val CONTENT_DESCRIPTION = ""
+
 @Composable
-fun FABComponent(
+fun ActionButtonComponent(
     ic: ImageVector,
     action: (() -> Unit)? = {},
 ) {
+    val colors = ButtonColors(
+        containerColor = getColors().primaryContainer,
+        contentColor = getColors().onPrimaryContainer,
+        disabledContainerColor = getColors().primaryContainer,
+        disabledContentColor = getColors().onPrimaryContainer,
+    )
     Button(
-        colors = ButtonColors(
-            containerColor = getColors().primaryContainer,
-            contentColor = getColors().onPrimaryContainer,
-            disabledContainerColor = getColors().primaryContainer,
-            disabledContentColor = getColors().onPrimaryContainer,
-        ),
+        colors = colors,
         modifier = Modifier
             .padding(16.dp)
             .size(64.dp),
-        onClick = {
-            action?.invoke()
-        }
+        onClick = { action?.invoke() }
     ) {
         Icon(
             imageVector = ic,
-            "",
+            contentDescription = CONTENT_DESCRIPTION,
         )
     }
 }
--- app/src/main/java/me/alexandervortex/shelfie/features/viewer/ViewerScreen.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/viewer/ViewerScreen.kt
@@ -1,29 +1,24 @@
 package me.alexandervortex.shelfie.features.viewer
 
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import me.alexandervortex.shelfie.ui.component.ComponentUI
-import me.alexandervortex.shelfie.ui.component.FABComponent
+import me.alexandervortex.shelfie.features.catalogue.ActionButtonComponent
 import me.alexandervortex.shelfie.ui.model.ElementUI
 import me.alexandervortex.shelfie.ui.theme.IC_ADD
 
@@ -71,27 +66,6 @@ fun ViewerScreen(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.BottomEnd
     ) {
-        if (true) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.Yellow)
-            ) {
-                val currentIndex = viewModel.INDEX_TO_AUTO_SCROLL.value
-                val currentPartIndex = viewModel.PART_INDEX_TO_HIGHLIGHT.value
-
-                Text(color = Color.Black, text = "${currentIndex}- ")
-                Text(color = Color.Black, text = " -${currentPartIndex}")
-
-                val currentItem = viewModel.bookModel.value?.elements?.getOrNull(currentIndex)
-                val currentPart = (currentItem as? ElementUI.TextUI)?.parts?.getOrNull(currentPartIndex)
-
-                Text(
-                    color = Color.Black,
-                    text = currentPart?.trim().orEmpty()
-                )
-            }
-        }
         // элементы книги
         LazyColumn(
             userScrollEnabled = viewModel.isScrollable.value,
@@ -119,7 +93,7 @@ fun ViewerScreen(
         }
 
         // плей-пауза кнопка
-        FABComponent(viewModel.buttonIcon?.value ?: IC_ADD) {
+        ActionButtonComponent(viewModel.buttonIcon?.value ?: IC_ADD) {
             viewModel.isScrollable.value = !viewModel.isScrollable.value
             // начинаю воспроизведение с первого (второго-третьего? видимого элемента)
             viewModel.togglePlayPause(
