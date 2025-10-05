0ac404fdd3a66ed729d0b9a033e41d386000baf6
Alexander Vortex <avorteks@alfabank.ru>
2025-10-05 14:13:31 +0200

    RDR-131 textui now storage words

--- app/src/main/java/me/alexandervortex/shelfie/ui/model/ElementUI.kt
+++ app/src/main/java/me/alexandervortex/shelfie/ui/model/ElementUI.kt
@@ -3,7 +3,7 @@ package me.alexandervortex.shelfie.ui.model
 sealed interface ElementUI {
 
     data class TextUI(
-        val text: String,
+        val words: List<String>,
     ) : ElementUI
 
     data class ImageUI(
