d49cadb7f0f7277a629e3044d08de5fb87fc8f43
Alexander Vortex <avorteks@alfabank.ru>
2025-10-10 00:45:22 +0200

    RDR-137 add empty state

--- app/src/main/java/me/alexandervortex/shelfie/features/catalogue/CatalogueScreen.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/catalogue/CatalogueScreen.kt
@@ -10,16 +10,21 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import me.alexandervortex.shelfie.data.db.entiry.BookEntity
 import me.alexandervortex.shelfie.features.catalogue.component.ActionButtonComponent
@@ -28,6 +33,7 @@ import me.alexandervortex.shelfie.features.catalogue.preview.CataloguePreviewDat
 import me.alexandervortex.shelfie.ui.component.BookComponent
 import me.alexandervortex.shelfie.ui.component.TitleComponent
 import me.alexandervortex.shelfie.ui.theme.IC_ADD
+import me.alexandervortex.shelfie.ui.theme.getColors
 
 @Composable
 @Preview(showBackground = true)
@@ -63,6 +69,19 @@ private fun CatalogueScreenContent(
                 ) {
                     TitleComponent(
                         text = "Your\nBooks"
+                    )
+                }
+            }
+            item {
+                if (uiState.books.isEmpty()) {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Start,
+                        color = getColors().onBackground,
+                        text = "Here is no books yet.\n\nAdd your first book.",
+                        fontSize = 32.sp,
+                        lineHeight = 32.sp,
+                        fontWeight = FontWeight.Thin,
                     )
                 }
             }
