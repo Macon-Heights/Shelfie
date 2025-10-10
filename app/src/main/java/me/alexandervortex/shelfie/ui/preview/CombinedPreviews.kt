1976a2340a524538192e480c73ee781547affb32
Alexander Vortex <avorteks@alfabank.ru>
2025-10-10 21:06:47 +0200

    RDR-137 refactor combined previews

--- app/src/main/java/me/alexandervortex/shelfie/ui/preview/CombinedPreviews.kt
+++ app/src/main/java/me/alexandervortex/shelfie/ui/preview/CombinedPreviews.kt
@@ -1,26 +1,17 @@
 package me.alexandervortex.shelfie.ui.preview
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import me.alexandervortex.shelfie.ui.theme.ShelfieTheme
 import me.alexandervortex.shelfie.ui.theme.getColors
 
-private const val WIDTH = 300
-private const val HEIGHT = 900
-
 @Preview(
-    widthDp = WIDTH,
-    heightDp = HEIGHT,
     name = "Light Mode",
     showBackground = true
 )
 @Preview(
-    widthDp = WIDTH,
-    heightDp = HEIGHT,
     name = "Dark Mode",
     showBackground = true,
     uiMode = Configuration.UI_MODE_NIGHT_YES
@@ -32,9 +23,6 @@ fun CombinedPreviews(
     content: @Composable () -> Unit,
 ) {
     ShelfieTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = getColors().background
-        ) { content.invoke() }
+        Surface(color = getColors().background) { content.invoke() }
     }
 }
