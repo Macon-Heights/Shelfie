cb9165c1f85f5ebf340b697d1c1cf2ee4ee14389
Alexander Vortex <avorteks@alfabank.ru>
2025-10-10 21:35:35 +0200

    RDR-137 create shapes file

---
+++ app/src/main/java/me/alexandervortex/shelfie/ui/theme/Shapes.kt
@@ -0,0 +1,6 @@
+package me.alexandervortex.shelfie.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+val SHAPE_M = RoundedCornerShape(16.dp)
