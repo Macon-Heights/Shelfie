4c6cd48e76fb3eac8dd1599f66435bf9371b3018
Alexander Vortex <avorteks@alfabank.ru>
2025-10-10 20:29:20 +0200

    RDR-137 add combined previews

---
+++ app/src/main/java/me/alexandervortex/shelfie/ui/preview/CombinedPreviews.kt
@@ -0,0 +1,33 @@
+package me.alexandervortex.shelfie.ui.preview
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import me.alexandervortex.shelfie.ui.theme.ShelfieTheme
+import me.alexandervortex.shelfie.ui.theme.getColors
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true
+)
+@Preview(
+    name = "Dark Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+annotation class CombinedPreviews
+
+@Composable
+fun CombinedPreviews(
+    content: @Composable () -> Unit,
+) {
+    ShelfieTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = getColors().background
+        ) { content.invoke() }
+    }
+}
