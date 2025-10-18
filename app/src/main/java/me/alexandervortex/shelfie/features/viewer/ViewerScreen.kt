2658536bf0c3bdb08531e9d66f4d373523fb1da2
Alexander Vortex <avorteks@alfabank.ru>
2025-10-18 20:19:37 +0200

    RDR-137 refactoring

--- app/src/main/java/me/alexandervortex/shelfie/features/viewer/ViewerScreen.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/viewer/ViewerScreen.kt
@@ -10,12 +10,16 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import me.alexandervortex.shelfie.R
 import me.alexandervortex.shelfie.features.mediaplayer.TtsViewModel
@@ -35,6 +39,7 @@ fun ViewerScreen(
 ) {
     val context = LocalContext.current
     val listState = rememberLazyListState()
+    val lifecycleOwner = LocalLifecycleOwner.current
     val book = viewModel.bookModel.value
 
     LaunchedEffect(true) {
@@ -44,11 +49,33 @@ fun ViewerScreen(
 
     LaunchedEffect(book) {
         Log.d("${TAG}_ViewerScreen", "loadBook:${book?.elements?.size}")
-        ttsVm.loadBook(book)
+        ttsVm.loadBook(book) // ttsViewModel.initTTSWithBook(it)
+//        ttsVm.state.value.index = book.progressIndex
+//        ttsVm.state.value.part = book.progressOffset
+
+        // old
+        viewModel.bookModel.value?.let {
+            listState.animateScrollToItem(ttsVm.state.value.index)
+        }
     }
 
     // state из сервиса реактивно
     val serviceState by ttsVm.state.collectAsStateWithLifecycle()
+
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_STOP) {
+                viewModel.saveScrollStateOnDispose(
+                    id = id,
+                    index = listState.firstVisibleItemIndex,
+                    offset = listState.firstVisibleItemScrollOffset
+                )
+            }
+
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose { lifecycleOwner.lifecycle.removeObserver(observer) }
+    }
 
     // UI
     Box(Modifier.fillMaxSize(), contentAlignment = Alignment.BottomEnd) {
@@ -70,17 +97,17 @@ fun ViewerScreen(
             }
         }
 
-            LaunchedEffect(serviceState.error) {
-                if (serviceState.error.isNotBlank()) {
+        LaunchedEffect(serviceState.error) {
+            if (serviceState.error.isNotBlank()) {
                 Log.e("${TAG}_ViewerScreen", "service_error:${serviceState.error}")
-                    Toast.makeText(context, serviceState.error, Toast.LENGTH_SHORT).show()
-                }
+                Toast.makeText(context, serviceState.error, Toast.LENGTH_SHORT).show()
             }
+        }
 
-            LaunchedEffect(serviceState.error) {
-                if (serviceState.error.isNotBlank()) {
+        LaunchedEffect(serviceState.error) {
+            if (serviceState.error.isNotBlank()) {
                 Log.e("${TAG}_ViewerScreen", "viewModel_error:${viewModel.errorState}")
-                    Toast.makeText(context, serviceState.error, Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, serviceState.error, Toast.LENGTH_SHORT).show()
             }
         }
 
