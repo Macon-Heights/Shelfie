7549735454564014c150df28a300603dbfe76800
Alexander Vortex <avorteks@alfabank.ru>
2025-10-18 13:42:26 +0200

    RDR-137 add logs

--- app/src/main/java/me/alexandervortex/shelfie/features/mediaplayer/TtsController.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/mediaplayer/TtsController.kt
@@ -3,8 +3,10 @@ package me.alexandervortex.shelfie.features.mediaplayer
 
 import android.content.Context
 import android.speech.tts.TextToSpeech
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import me.alexandervortex.shelfie.R
+import me.alexandervortex.shelfie.features.viewer.TAG
 import me.alexandervortex.shelfie.ui.model.BookUI
 import me.alexandervortex.shelfie.ui.model.ElementUI
 import java.util.Locale
@@ -24,11 +26,23 @@ class TtsController(
     val buttonIcon = mutableStateOf(R.drawable.ic_play)
 
     override fun onInit(status: Int) {
+        Log.d(
+            "${TAG}TtsController",
+            "onInit:$status"
+        )
         val locale = bookModel?.titleInfo?.lang?.let(::Locale) ?: Locale.getDefault()
         if (status == TextToSpeech.SUCCESS) {
+            Log.d(
+                "${TAG}_TtsController",
+                "TextToSpeech.SUCCESS"
+            )
             val result = tts?.setLanguage(locale)
             if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                 onAppError("Язык не поддерживается")
+                Log.e(
+                    "${TAG}_TtsController",
+                    "Язык не поддерживается"
+                )
             }
             tts?.setOnUtteranceProgressListener(object :
                 android.speech.tts.UtteranceProgressListener() {
@@ -42,21 +56,43 @@ class TtsController(
                     }
                 }
 
-                override fun onDone(utteranceId: String?) {}
+                override fun onDone(utteranceId: String?) {
+                    Log.d(
+                        "${TAG}_TtsController",
+                        "onDone:$utteranceId"
+                    )
+                }
+
                 override fun onError(utteranceId: String?) {
-                    onAppError("Ошибка TTS utterance $utteranceId")
+                    Log.d(
+                        "${TAG}_TtsController",
+                        "Ошибка TTS: $utteranceId"
+                    )
+                    onAppError("Ошибка TTS: $utteranceId")
                 }
             })
         } else {
+            Log.d(
+                "${TAG}_TtsController",
+                "Ошибка инициализации TTS"
+            )
             onAppError("Ошибка инициализации TTS")
         }
     }
 
     fun togglePlayPause(indexToStartPlaying: Int) {
+        Log.d(
+            "${TAG}_TtsController",
+            "togglePlayPause:${indexToStartPlaying}:${isSpeaking.value}"
+        )
         if (isSpeaking.value) stopSpeaking() else startSpeaking(indexToStartPlaying)
     }
 
     fun release() {
+        Log.d(
+            "${TAG}_TtsController",
+            "release"
+        )
         tts?.stop()
         tts?.shutdown()
     }
@@ -89,6 +125,10 @@ class TtsController(
     }
 
     private fun stopSpeaking() {
+        Log.d(
+            "${TAG}_TtsController",
+            "stopSpeaking"
+        )
         tts?.stop()
         isSpeaking.value = false
         buttonIcon.value = R.drawable.ic_play
