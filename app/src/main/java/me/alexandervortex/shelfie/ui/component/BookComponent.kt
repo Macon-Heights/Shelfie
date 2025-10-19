32f1023353fe6aadde65d476707fb3c321f52dae
Alexander Vortex <avorteks@alfabank.ru>
2025-10-19 14:27:50 +0200

    RDR-140 replace year with progress on catalogue view

--- app/src/main/java/me/alexandervortex/shelfie/ui/component/BookComponent.kt
+++ app/src/main/java/me/alexandervortex/shelfie/ui/component/BookComponent.kt
@@ -12,8 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import me.alexandervortex.shelfie.data.db.entiry.BookEntity
 import me.alexandervortex.shelfie.features.catalogue.ui.preview.CataloguePreviewData
 import me.alexandervortex.shelfie.ui.theme.SHAPE_M
@@ -27,7 +30,6 @@ fun BookComponent(
     val color = getColors().surfaceVariant
     val onColor = getColors().onSurfaceVariant
     val onColorForTitle = getColors().onSurface
-
     Column(
         modifier = modifier
             .shadow(
@@ -37,101 +39,47 @@ fun BookComponent(
             )
             .clip(SHAPE_M)
             .background(color)
-            .fillMaxWidth()
             .padding(16.dp)
     ) {
         model.title?.let {
             Text(
-                it,
+                model.title,
                 color = onColorForTitle,
+                fontSize = 18.sp,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Left
             )
         }
-
-        Spacer(Modifier.size(16.dp))
-
-        model.id.let {
-            Text(
-                "id: $it",
-                color = onColorForTitle,
-
-                )
-        }
-
-        model.author?.let {
-            Text(
-                "author: ${it}",
-                color = onColorForTitle,
-            )
-        }
-        model.year?.let {
-            Text(
-                "year: ${it}",
-                color = onColorForTitle,
-            )
-        }
-        Row {
-            Text(
-                "- index: ${model.scrollIndex}",
-                color = onColorForTitle,
-
-                )
-            Text(
-                "- offset: ${model.scrollOffset}",
-                color = onColorForTitle,
-            )
-        }
-
-    }
-
-    /*    Column(
-            modifier = modifier
-                .shadow(
-                    elevation = 6.dp,
-                    shape = SHAPE_M,
-                    clip = false
-                )
-                .clip(SHAPE_M)
-                .background(color)
-                .padding(16.dp)
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
         ) {
-            model.title?.let {
-                Text(
-                    model.title,
-                    color = onColorForTitle,
-                    fontSize = 18.sp,
-                    modifier = Modifier.fillMaxWidth(),
-                    textAlign = TextAlign.Left
-                )
-            }
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(top = 16.dp)
-            ) {
-                model.year?.let {
-                    Text(
-                        color = onColor,
-                        fontWeight = FontWeight.Light,
-                        text = it,
-                        textAlign = TextAlign.End
-                    )
-                }
-                Spacer(
-                    Modifier
-                        .size(16.dp)
-                        .weight(1f)
-                )
-                model.author?.let {
-                    Text(
-                        color = onColor,
-                        fontWeight = FontWeight.Light,
-                        text = it,
-                        textAlign = TextAlign.End
-                    )
-                }
 
+            if (model.scrollIndex > 0) {
+                Text(
+                    color = onColor,
+                    fontWeight = FontWeight.Light,
+                    text = "Section: ${model.scrollIndex}",
+                    textAlign = TextAlign.End
+                )
             }
-        }*/
+            Spacer(
+                Modifier
+                    .size(16.dp)
+                    .weight(1f)
+            )
+            model.author?.let {
+                Text(
+                    color = onColor,
+                    fontWeight = FontWeight.Light,
+                    text = it,
+                    textAlign = TextAlign.End
+                )
+            }
+
+        }
+    }
 }
 
 @Composable
