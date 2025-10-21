a96a4199a437d64d91a6a4f081974eae61bedbf9
Alexander Vortex <avorteks@alfabank.ru>
2025-10-21 19:33:19 +0200

    RDR-140 create service state component

--- app/src/main/java/me/alexandervortex/shelfie/ui/theme/Shapes.kt
+++ app/src/main/java/me/alexandervortex/shelfie/ui/theme/Shapes.kt
@@ -3,6 +3,7 @@ package me.alexandervortex.shelfie.ui.theme
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.unit.dp
 
+val SHAPE_S = RoundedCornerShape(8.dp)
 val SHAPE_M = RoundedCornerShape(16.dp)
 val SHAPE_L = RoundedCornerShape(32.dp)
 val SHAPE_XL = RoundedCornerShape(64.dp)
