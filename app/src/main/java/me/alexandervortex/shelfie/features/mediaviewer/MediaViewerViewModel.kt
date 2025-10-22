2a449ba06a687a9d6350263f70168a4f354329f2
Alexander Vortex <avorteks@alfabank.ru>
2025-10-22 20:28:14 +0200

    RDR-140 backup of viewer view model (also need to fix service and mb copy of tts)

---
+++ app/src/main/java/me/alexandervortex/shelfie/features/mediaviewer/MediaViewerViewModel.kt
@@ -0,0 +1,152 @@
+package me.alexandervortex.shelfie.features.mediaviewer
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
+import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import me.alexandervortex.shelfie.data.repository.BookRepository
+import me.alexandervortex.shelfie.features.mediaplayer.PlayerService
+import me.alexandervortex.shelfie.features.mediaplayer.ServiceState
+import me.alexandervortex.shelfie.features.viewer.TAG
+import me.alexandervortex.shelfie.ui.model.BookUI
+import javax.inject.Inject
+
+@HiltViewModel
+class MediaViewerViewModel
+@Inject constructor(
+    private val repo: BookRepository,
+) : ViewModel() {
+
+    val errorState = mutableStateOf("")
+    val bookUI: MutableState<BookUI?> = mutableStateOf(null)
+
+    private var service: MediaService? = null
+    private var serviceJob: Job? = null
+    private var isBound = false
+    private val _state = MutableStateFlow(ServiceState())
+    val state: StateFlow<ServiceState> = _state.asStateFlow()
+
+    private val conn = object : ServiceConnection {
+
+        override fun onServiceConnected(name: ComponentName?, binder: IBinder?) {
+            Log.d(
+                "${TAG}_MediaViewerViewModel",
+                "onServiceConnected:${name?.className}:${binder?.isBinderAlive}"
+            )
+            val newService = (binder as? MediaService.LocalBinder)?.getService() ?: return
+            service = newService
+            isBound = true
+
+            serviceJob?.cancel() // check later
+            serviceJob = viewModelScope.launch {
+                newService.state.collect {
+                    Log.d("${TAG}_MediaViewerViewModel", "state_collect:${_state.value}:${it}")
+                    _state.value = it
+                }
+            }
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            Log.d("${TAG}_MediaViewerViewModel", "onServiceDisconnected:${name}")
+            serviceJob?.cancel()
+            serviceJob = null
+            service = null
+            isBound = false
+        }
+    }
+
+    fun bindService(context: Context) {
+        if (isBound) return
+        val intent = Intent(context, PlayerService::class.java)
+        // гарантируем живой сервис
+        context.startForegroundService(intent)
+        context.bindService(intent, conn, Context.BIND_AUTO_CREATE)
+        isBound = true
+    }
+
+    /** вызывать при dispose или onStop */
+    fun unbindService(context: Context) {
+        if (!isBound) return
+        Log.d("${TAG}_MediaViewerViewModel", "unbindService")
+        runCatching {
+            Log.d(
+                "${TAG}_MediaViewerViewModel",
+                "runCatching"
+            )
+            context.unbindService(conn)
+        }.onFailure { Log.e("${TAG}_MediaViewerViewModel", "unbindService failed", it) }
+        serviceJob?.cancel()
+        serviceJob = null
+        service = null
+        isBound = false
+    }
+
+    fun loadCurrentBook(id: String) {
+        viewModelScope.launch {
+            try {
+                // screenBook выставлен, книга загружена и готова к чтению
+                val book = repo.getBookModelById(id)
+                // сразу же отображаю ее на экране, чтобы в любом из кейсов пользователь начал ее листать
+                bookUI.value = book
+
+                // нажали книгу в каталоге
+                // она загружается из памяти и вгружается в сервис, чтобы воспроизводиться
+                // когда она воспроизводится, она уже в сервисе, а так же в репозитории
+                // когда я выгружаю приложение из памяти, у меня создается новая вьюмодель
+                // новая вьюмодель цепляется к существующему сервису и проверяет, ждал ли он ее
+                // если он ее не ждал и был занят воспроизведением - если это та же книга - окей, продолжаем
+                // если это другая книга, то продолжаем воспроизводить старую (отображая ее название внизу)
+                // как теперь мне воспроизвести текущую книгу, если плеер уже занят другой?
+                //
+                //
+                // сделать кнопку точкой входа в плеер?
+                // нажимаю воспроизвести - открывается плажка с кнопками скорости, пауза, переключение глав, прогрессом
+                // нажимаю паузу и все сворачивается в одну кнопочку "плей" занаво?
+
+                // каррентбук должен сохраняться в плеер при нажатии ПЛЕЙ
+                // если сделать заранее - то при прослушивании книги А -
+                // ты зайдешь на книгу Б и перезатрешь "currentBook"
+                // а значит придется проверять книга на экране и книга в сервисе - это одна книга? (В сервисе)
+            } catch (e: Exception) {
+                errorState.value = e.localizedMessage ?: "unknown viewmodel error"
+            }
+        }
+    }
+
+    fun saveScrollStateOnDispose(
+        id: String,
+        index: Int,
+        offset: Int,
+    ) {
+        viewModelScope.launch {
+            repo.saveCurrentBookProgress(id, index, offset)
+        }
+    }
+
+    fun togglePlayPause(currentTopIndex: Int) {
+        Log.d("${TAG}_MediaViewerViewModel", "togglePlayPause:${currentTopIndex}")
+        // BUT
+        // I NEED TO CREATE SERVICE AND ADD BOOK ?
+        service?.loadBook(bookUI.value)
+        service?.togglePlayPause(currentTopIndex)
+    }
+
+    fun loadBook(book: BookUI) {
+        Log.d("${TAG}_MediaViewerViewModel", "loadBook:${book.elements.size}")
+        bookUI = book // with progress index
+//        _state.value = state.value.copy(index = book.progressIndex)
+
+    }
+}
