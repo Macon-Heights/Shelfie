6238e7e66bc8f8d9a2f61a88235594f6d493cd87
Alexander Vortex <avorteks@alfabank.ru>
2025-10-24 01:58:40 +0200

    RDR-140 add nice settings buttons

--- app/src/main/java/me/alexandervortex/shelfie/features/mediaviewer/MediaStateComponent.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/mediaviewer/MediaStateComponent.kt
@@ -1,12 +1,15 @@
 package me.alexandervortex.shelfie.features.mediaviewer
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,12 +17,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import me.alexandervortex.shelfie.ui.component.ActionButtonComponent
 import me.alexandervortex.shelfie.ui.theme.IC_PAUSE
-import me.alexandervortex.shelfie.ui.theme.IC_PLAY
+import me.alexandervortex.shelfie.ui.theme.IC_SPEED
+import me.alexandervortex.shelfie.ui.theme.IC_TIMER
+import me.alexandervortex.shelfie.ui.theme.SHAPE_L
 import me.alexandervortex.shelfie.ui.theme.SHAPE_M
 import me.alexandervortex.shelfie.ui.theme.getColors
 
@@ -28,76 +34,99 @@ fun ServiceStateComponent(
     state: MediaServiceState,
     playPauseAction: () -> Unit,
 ) {
-    if (state.isPlaying) {
-        Row(
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .shadow(
+                elevation = 6.dp,
+                shape = SHAPE_M,
+                clip = false
+            )
+            .clip(SHAPE_M)
+            .background(getColors().primaryContainer)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            Modifier.weight(1f)
+        ) {
+            Text(
+                text = state.title.orEmpty(),
+                color = getColors().onPrimaryContainer,
+                fontSize = 18.sp,
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Spacer(Modifier.size(8.dp))
+            Text(
+                color = getColors().onPrimaryContainer,
+                fontWeight = FontWeight.Light,
+                text = state.author.orEmpty(),
+            )
+        }
+        Spacer(Modifier.size(16.dp))
+        ActionButtonComponent(
+            content = {
+                Icon(
+                    imageVector = IC_PAUSE,
+                    contentDescription = null
+                )
+            },
+            action = { playPauseAction.invoke() }
+        )
+        Spacer(Modifier.size(16.dp))
+        Button(
+            shape = SHAPE_M,
+            contentPadding = PaddingValues(0.dp),
+            colors = ButtonColors(
+                containerColor = getColors().primary,
+                contentColor = getColors().onPrimary,
+                disabledContainerColor = getColors().primary,
+                disabledContentColor = getColors().onPrimary,
+            ),
             modifier = Modifier
-                .fillMaxWidth()
-                .height(150.dp)
-                .padding(16.dp)
+                .size(48.dp)
                 .shadow(
-                    elevation = 6.dp,
-                    shape = SHAPE_M,
+                    elevation = 8.dp,
+                    shape = SHAPE_L,
                     clip = false
                 )
-                .clip(SHAPE_M)
-                .background(getColors().primary)
-                .padding(16.dp)
-        ) {
-            Column(
-                Modifier.weight(1f)
-            ) {
-                Text(
-                    text = "is playing",
-                    color = getColors().onPrimary
-                )
-                Text("${state.index}:${state.part}", color = getColors().onPrimary)
-            }
+                .clip(SHAPE_L),
+            onClick = {
 
-            ActionButtonComponent(
-                content = {
-                    Icon(
-                        imageVector = IC_PAUSE,
-                        contentDescription = null
-                    )
-                },
-                action = { playPauseAction.invoke() }
-            )
-            Column(
-                Modifier.weight(1f),
-                horizontalAlignment = Alignment.End
-            ) {
-                Text(
-                    color = getColors().onPrimary,
-                    textAlign = TextAlign.End,
-                    text = state.title.toString()
-                )
-                Text(
-                    color = getColors().onPrimary,
-                    textAlign = TextAlign.End,
-                    text = state.author.toString()
-                )
             }
+        ) {
+            Icon(
+                imageVector = IC_SPEED,
+                contentDescription = null
+            )
         }
-    } else {
-        Row(
+        Spacer(Modifier.size(16.dp))
+        Button(
+            shape = SHAPE_M,
+            contentPadding = PaddingValues(0.dp),
+            colors = ButtonColors(
+                containerColor = getColors().primary,
+                contentColor = getColors().onPrimary,
+                disabledContainerColor = getColors().primary,
+                disabledContentColor = getColors().onPrimary,
+            ),
             modifier = Modifier
-                .fillMaxWidth()
-                .height(150.dp)
-                .padding(32.dp)
+                .size(48.dp)
+                .shadow(
+                    elevation = 8.dp,
+                    shape = SHAPE_L,
+                    clip = false
+                )
+                .clip(SHAPE_L),
+            onClick = {
+
+            }
         ) {
-            Box(Modifier.weight(1f))
-
-            ActionButtonComponent(
-                content = {
-                    Icon(
-                        imageVector = IC_PLAY,
-                        contentDescription = null
-                    )
-                },
-                action = { playPauseAction.invoke() }
+            Icon(
+                imageVector = IC_TIMER,
+                contentDescription = null
             )
-
-            Box(Modifier.weight(1f))
         }
     }
 }
--- app/src/main/java/me/alexandervortex/shelfie/ui/component/ActionButtonComponent.kt
+++ app/src/main/java/me/alexandervortex/shelfie/ui/component/ActionButtonComponent.kt
@@ -15,8 +15,6 @@ import me.alexandervortex.shelfie.ui.theme.SHAPE_L
 import me.alexandervortex.shelfie.ui.theme.SHAPE_M
 import me.alexandervortex.shelfie.ui.theme.getColors
 
-private const val CONTENT_DESCRIPTION = ""
-
 @Composable
 @CombinedPreviews
 fun ActionButtonComponentPreview() {
@@ -35,16 +33,15 @@ fun ActionButtonComponent(
     content: @Composable () -> Unit,
     action: (() -> Unit)? = {},
 ) {
-    val colors = ButtonColors(
-        containerColor = getColors().primaryContainer,
-        contentColor = getColors().onPrimaryContainer,
-        disabledContainerColor = getColors().primaryContainer,
-        disabledContentColor = getColors().onPrimaryContainer,
-    )
 
     Button(
         shape = SHAPE_M,
-        colors = colors,
+        colors = ButtonColors(
+            containerColor = getColors().primary,
+            contentColor = getColors().onPrimary,
+            disabledContainerColor = getColors().primary,
+            disabledContentColor = getColors().onPrimary,
+        ),
         modifier = Modifier
             .size(64.dp)
             .shadow(
@@ -54,7 +51,5 @@ fun ActionButtonComponent(
             )
             .clip(SHAPE_L),
         onClick = { action?.invoke() }
-    ) {
-        content.invoke()
-    }
+    ) { content.invoke() }
 }
