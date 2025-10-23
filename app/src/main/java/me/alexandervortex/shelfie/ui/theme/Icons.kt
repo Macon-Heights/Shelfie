ac7b32d72c1f35dc8a80f674d7c6c285bc4175a9
Alexander Vortex <avorteks@alfabank.ru>
2025-10-24 01:58:40 +0200

    RDR-140 add nice settings buttons

--- app/src/main/java/me/alexandervortex/shelfie/ui/theme/Icons.kt
+++ app/src/main/java/me/alexandervortex/shelfie/ui/theme/Icons.kt
@@ -1,11 +1,14 @@
 package me.alexandervortex.shelfie.ui.theme
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Pause
-import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Pause
+import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.material.icons.outlined.SettingsVoice
+import androidx.compose.material.icons.outlined.Speed
+import androidx.compose.material.icons.outlined.Timer
 
-private val icons = Icons.Default
+private val icons = Icons.Outlined
 
 // Catalogue
 val IC_ADD = icons.Add
@@ -13,4 +16,9 @@ val IC_ADD = icons.Add
 // Player
 val IC_PLAY = icons.PlayArrow
 val IC_PAUSE = icons.Pause
+
+val IC_SPEED = icons.Speed
+val IC_VOICE = icons.SettingsVoice
+val IC_TIMER = icons.Timer
+
 
