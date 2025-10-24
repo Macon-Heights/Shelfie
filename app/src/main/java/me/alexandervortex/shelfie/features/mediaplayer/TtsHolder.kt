e116b858968b394f16f33e73a0d2f9e2fab6aa8f
Alexander Vortex <avorteks@alfabank.ru>
2025-10-24 21:25:58 +0200

    RDR-140 remove deprecated classes

--- app/src/main/java/me/alexandervortex/shelfie/features/mediaplayer/TtsHolder.kt
+++
@@ -1,9 +0,0 @@
-package me.alexandervortex.shelfie.features.mediaplayer
-
-import me.alexandervortex.shelfie.ui.model.BookUI
-
-@Deprecated("GAVNO")
-object ShelfieTtsHolder {
-
-    var currentBook: BookUI? = null
-}
