50757672e75b2865873a93e9f33025660f5480c5
Alexander Vortex <avorteks@alfabank.ru>
2025-10-25 18:48:41 +0200

    RDR-140 make lil refactoring

--- app/src/main/java/me/alexandervortex/shelfie/features/mediaviewer/TimerValue.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/mediaviewer/TimerValue.kt
@@ -2,7 +2,8 @@ package me.alexandervortex.shelfie.features.mediaviewer
 
 enum class TimerValue(val mins: Int, val text: String) {
 
-    MIN_ZERO(0, ""),
+    OFF(0, ""),
+    TEST(2, "2m"),
     MIN_20(20, "20m"),
     MIN_40(40, "40m"),
     HOUR(60, "1h");
