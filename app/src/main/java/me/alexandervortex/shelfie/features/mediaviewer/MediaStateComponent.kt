09cce0df522b1f404a75fef6ca84fc51e2472ca6
Alexander Vortex <avorteks@alfabank.ru>
2025-10-25 16:28:57 +0200

    RDR-140 add labels

--- app/src/main/java/me/alexandervortex/shelfie/features/mediaviewer/MediaStateComponent.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/mediaviewer/MediaStateComponent.kt
@@ -8,14 +8,17 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import me.alexandervortex.shelfie.ui.component.getBookUI
 import me.alexandervortex.shelfie.ui.preview.CombinedPreviews
 import me.alexandervortex.shelfie.ui.theme.IC_PLAYER_NEXT
 import me.alexandervortex.shelfie.ui.theme.IC_PLAYER_PAUSE
@@ -23,7 +26,7 @@ import me.alexandervortex.shelfie.ui.theme.IC_PLAYER_PLAY
 import me.alexandervortex.shelfie.ui.theme.IC_PLAYER_PREV
 import me.alexandervortex.shelfie.ui.theme.IC_PLAYER_SPEED
 import me.alexandervortex.shelfie.ui.theme.IC_PLAYER_TIMER
-import me.alexandervortex.shelfie.ui.theme.SHAPE_L
+import me.alexandervortex.shelfie.ui.theme.SHAPE_TOP_L
 import me.alexandervortex.shelfie.ui.theme.getColors
 
 @Composable
@@ -39,16 +42,16 @@ fun MediaStateComponent(
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(bottom = 12.dp)
-            .padding(8.dp)
+//            .padding(bottom = 12.dp)
+//            .padding(8.dp)
             .shadow(
                 elevation = 6.dp,
-                shape = SHAPE_L,
+                shape = SHAPE_TOP_L,
                 clip = false
             )
-            .clip(SHAPE_L)
+            .clip(SHAPE_TOP_L)
             .background(getColors().surfaceBright)
-            .padding(8.dp),
+//            .padding(16.dp),
     ) {
         Text(
             modifier = Modifier
@@ -59,20 +62,35 @@ fun MediaStateComponent(
             text = state.title.orEmpty(),
             color = getColors().onSurface,
         )
-        Spacer(Modifier.size(8.dp))
         ProgressLine()
-        Spacer(Modifier.size(12.dp))
         Row(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.Top,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             val playPauseIcon = if (state.isPlaying) IC_PLAYER_PAUSE else IC_PLAYER_PLAY
-            RoundButton(IC_PLAYER_TIMER) { timerAction.invoke() }
-            RoundButton(IC_PLAYER_PREV) { prevAction.invoke() }
+            ButtonWithLabel(IC_PLAYER_PREV) { prevAction.invoke() }
+            ButtonWithLabel(IC_PLAYER_TIMER, "30m") { timerAction.invoke() }
             RoundButton(playPauseIcon, true) { playPauseAction.invoke() }
-            RoundButton(IC_PLAYER_NEXT) { nextAction.invoke() }
-            RoundButton(IC_PLAYER_SPEED) { speedAction.invoke() }
+            ButtonWithLabel(IC_PLAYER_SPEED, state.speed.text) { speedAction.invoke() }
+            ButtonWithLabel(IC_PLAYER_NEXT) { nextAction.invoke() }
+        }
+    }
+}
+
+@Composable
+private fun ButtonWithLabel(
+    playPauseIcon: ImageVector,
+    text: String? = null,
+    playPauseAction: () -> Unit,
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        RoundButton(playPauseIcon) { playPauseAction.invoke() }
+        if (text.isNullOrEmpty().not()) {
+            Spacer(Modifier.size(8.dp))
+            Text(text = text.orEmpty(), fontSize = 14.sp, lineHeight = 14.sp)
         }
     }
 }
@@ -85,7 +103,7 @@ fun MediaViewerPreview2() {
         MediaViewerContent(
             isMenu = true,
             book = bookUI,
-            serviceState = MediaServiceState.playingState(),
+            serviceState = MediaServiceState.playingState().copy(speed = SpeechRate.FAST),
             listState = LazyListState(),
             {}, {}, {}, {}, {}, {},
         )
--- app/src/main/java/me/alexandervortex/shelfie/features/mediaviewer/SpeechRate.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/mediaviewer/SpeechRate.kt
@@ -1,10 +1,10 @@
 package me.alexandervortex.shelfie.features.mediaviewer
 
-enum class SpeechRate(val speed: Float) {
+enum class SpeechRate(val speed: Float, val text: String) {
 
-    DEFAULT(1f),
-    FAST(1.5f),
-    FASTEST(2f);
+    DEFAULT(1f, "1x"),
+    FAST(1.5f, "1.5x"),
+    FASTEST(2f, "2x");
 
     fun getNext(): SpeechRate {
         val nextIndex = (ordinal + 1) % entries.size
