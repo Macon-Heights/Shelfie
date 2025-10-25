bc76cad7dc665e8ee9c8d9fb04d692b35f4ed5fc
Alexander Vortex <avorteks@alfabank.ru>
2025-10-26 01:22:43 +0200

    RDR-143 add elements and db migration

--- app/src/main/java/me/alexandervortex/shelfie/data/db/BookDatabase.kt
+++ app/src/main/java/me/alexandervortex/shelfie/data/db/BookDatabase.kt
@@ -2,6 +2,9 @@ package me.alexandervortex.shelfie.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import me.alexandervortex.shelfie.data.db.dao.BOOK_TABLE
 import me.alexandervortex.shelfie.data.db.dao.BookDao
 import me.alexandervortex.shelfie.data.db.entiry.BookEntity
 
@@ -9,4 +12,14 @@ import me.alexandervortex.shelfie.data.db.entiry.BookEntity
 abstract class BookDatabase : RoomDatabase() {
 
     abstract fun bookDao(): BookDao
+
+    companion object {
+
+        private val NEW_FIELD = "elements"
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE $BOOK_TABLE ADD COLUMN $NEW_FIELD INTEGER NOT NULL DEFAULT 0")
+            }
+        }
+    }
 }
