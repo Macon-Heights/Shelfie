8a1e146bf330eb9e14f19a782d87e98e9c4b2f03
Alexander Vortex <avorteks@alfabank.ru>
2025-10-25 16:21:02 +0200

    RDR-140 add new shape

--- app/src/main/java/me/alexandervortex/shelfie/ui/theme/Shapes.kt
+++ app/src/main/java/me/alexandervortex/shelfie/ui/theme/Shapes.kt
@@ -8,3 +8,5 @@ val SHAPE_M = RoundedCornerShape(16.dp)
 val SHAPE_L = RoundedCornerShape(32.dp)
 val SHAPE_XL = RoundedCornerShape(64.dp)
 val SHAPE_XXL = RoundedCornerShape(128.dp)
+
+val SHAPE_TOP_L = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp)
