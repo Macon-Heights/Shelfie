962ae354869f9bdd8120ed5bc88533313344a388
Alexander Vortex <avorteks@alfabank.ru>
2025-10-26 15:04:44 +0100

    RDR-143 fix player padding

--- app/src/main/java/me/alexandervortex/shelfie/features/mediaviewer/MediaStateComponent.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/mediaviewer/MediaStateComponent.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -50,8 +53,9 @@ fun MediaStateComponent(
             )
             .clip(SHAPE_TOP_L)
             .background(getColors().surfaceVariant)
-            .padding(12.dp)
-            .padding(bottom = 4.dp),
+            .padding(top = 12.dp)
+            .padding(horizontal = 12.dp)
+            .windowInsetsPadding(WindowInsets.navigationBars)
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
