01056b26a4ac7235b3d68ca0eec4891a5ebca825
Alexander Vortex <avorteks@alfabank.ru>
2025-10-27 20:36:48 +0100

    RDR-143 save book component before refactoring

--- app/src/main/java/me/alexandervortex/shelfie/ui/component/BookComponent.kt
+++ app/src/main/java/me/alexandervortex/shelfie/ui/component/BookComponent.kt
@@ -1,16 +1,20 @@
 package me.alexandervortex.shelfie.ui.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -19,6 +23,8 @@ import me.alexandervortex.shelfie.data.db.entiry.BookEntity
 import me.alexandervortex.shelfie.features.catalogue.ui.preview.CataloguePreviewData
 import me.alexandervortex.shelfie.features.mediaviewer.ProgressLine
 import me.alexandervortex.shelfie.ui.preview.CombinedPreviews
+import me.alexandervortex.shelfie.ui.theme.IC_CHECK
+import me.alexandervortex.shelfie.ui.theme.IC_UNCHECK
 import me.alexandervortex.shelfie.ui.theme.SHAPE_M
 import me.alexandervortex.shelfie.ui.theme.getColors
 
@@ -26,43 +32,57 @@ import me.alexandervortex.shelfie.ui.theme.getColors
 fun BookComponent(
     model: BookEntity,
     modifier: Modifier = Modifier,
+    isChecked: Boolean? = null, // я начал делать, но заплутал чуть чуть в логике
 ) {
     val color = getColors().surfaceVariant
     val onColor = getColors().onSurfaceVariant
     val onColorForTitle = getColors().onSurface
-    Column(
-        modifier = modifier
-            .shadow(
-                elevation = 6.dp,
-                shape = SHAPE_M,
-                clip = false
-            )
-            .clip(SHAPE_M)
-            .background(color)
-            .padding(16.dp)
+    Row(
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        model.title?.let {
-            Text(
-                text = it,
-                color = onColorForTitle,
-                fontSize = 18.sp,
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Left
+        if (isChecked != null) {
+            Image(
+                modifier = Modifier.size(32.dp),
+                imageVector = if (isChecked) IC_CHECK else IC_UNCHECK,
+                contentDescription = "",
+                colorFilter = ColorFilter.tint(getColors().primary)
             )
+            Spacer(Modifier.size(16.dp))
         }
-        Spacer(Modifier.size(4.dp))
-        model.author?.let {
-            Text(
-                text = it,
-                color = onColor,
-                fontWeight = FontWeight.Light,
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.End
-            )
-        }
-        if (model.scrollIndex > 0) {
-            Spacer(Modifier.size(8.dp))
-            ProgressLine(model.scrollIndex, model.elements)
+        Column(
+            modifier = modifier
+                .shadow(
+                    elevation = 6.dp,
+                    shape = SHAPE_M,
+                    clip = false
+                )
+                .clip(SHAPE_M)
+                .background(color)
+                .padding(16.dp)
+        ) {
+            model.title?.let {
+                Text(
+                    text = it,
+                    color = onColorForTitle,
+                    fontSize = 18.sp,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Left
+                )
+            }
+            Spacer(Modifier.size(4.dp))
+            model.author?.let {
+                Text(
+                    text = it,
+                    color = onColor,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.End
+                )
+            }
+            if (model.scrollIndex > 0) {
+                Spacer(Modifier.size(8.dp))
+                ProgressLine(model.scrollIndex, model.elements)
+            }
         }
     }
 }
@@ -80,5 +100,7 @@ fun BookComponent() {
         scrollIndex = 46,
         elements = 100
     )
-    BookComponent(kek)
+    CombinedPreviews {
+        BookComponent(kek, isChecked = false)
+    }
 }
