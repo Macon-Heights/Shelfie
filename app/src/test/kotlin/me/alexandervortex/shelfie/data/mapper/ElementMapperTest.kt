d3177002e11fb97af275e407fd7a1fe349088569
Alexander Vortex <avorteks@alfabank.ru>
2025-11-02 12:34:43 +0100

    RDR-143 create emty test and testdata

---
+++ app/src/test/kotlin/me/alexandervortex/shelfie/data/mapper/ElementMapperTest.kt
@@ -0,0 +1,7 @@
+package me.alexandervortex.shelfie.data.mapper
+
+import io.kotest.core.spec.style.FunSpec
+
+class ElementMapperTest : FunSpec({
+
+})
---
+++ app/src/test/kotlin/me/alexandervortex/shelfie/data/mapper/ElementMapperTestData.kt
@@ -0,0 +1,5 @@
+package me.alexandervortex.shelfie.data.mapper
+
+object ElementMapperTestData {
+
+}
