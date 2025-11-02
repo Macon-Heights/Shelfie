9a952acc80a83330a768c19005c80d2394e529bc
Alexander Vortex <avorteks@alfabank.ru>
2025-11-02 15:12:41 +0100

    RDR-159 and first broken test

--- app/src/test/kotlin/me/alexandervortex/shelfie/data/mapper/ElementMapperTest.kt
+++ app/src/test/kotlin/me/alexandervortex/shelfie/data/mapper/ElementMapperTest.kt
@@ -1,7 +1,24 @@
 package me.alexandervortex.shelfie.data.mapper
 
-import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import me.alexandervortex.shelfie.data.mapper.ElementMapperXmlTestData.toBody
 
-class ElementMapperTest : FunSpec({
+class ElementMapperTest : BehaviorSpec({
 
+    val mapper = ElementMapper()
+    val binaries = ElementMapperXmlTestData.binaries()
+
+    Given("a paragraph with text and image") {
+        val body = ElementMapperXmlTestData.paragraphWithTextAndImage().toBody()
+        val model = ElementMapperTestData.paragraphWithTextAndImage()
+
+        When("test") {
+            val result = mapper.map(body, binaries)
+
+            Then("test") {
+                result shouldBe model
+            }
+        }
+    }
 })
