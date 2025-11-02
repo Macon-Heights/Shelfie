e15b6ff2ed32c0b1293fb7608920c4efca27ccf6
Alexander Vortex <avorteks@alfabank.ru>
2025-11-02 15:12:32 +0100

    RDR-159 create test data files

--- app/src/test/kotlin/me/alexandervortex/shelfie/data/mapper/ElementMapperTestData.kt
+++ app/src/test/kotlin/me/alexandervortex/shelfie/data/mapper/ElementMapperTestData.kt
@@ -1,5 +1,14 @@
 package me.alexandervortex.shelfie.data.mapper
 
+import me.alexandervortex.shelfie.ui.model.ElementUI
+
 object ElementMapperTestData {
 
+    fun paragraphWithTextAndImage(): List<ElementUI> {
+        return listOf(
+            ElementUI.TextUI(listOf("Перед картинкой — текст.")),
+            ElementUI.ImageUI("img1".toByteArray()),
+            ElementUI.TextUI(listOf("После картинки — текст.")),
+        )
+    }
 }
