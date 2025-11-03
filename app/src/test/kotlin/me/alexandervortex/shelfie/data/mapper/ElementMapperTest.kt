7e2dfcecef4b04c150b69cca2364586ad5be8997
Alexander Vortex <avorteks@alfabank.ru>
2025-11-03 13:42:15 +0100

    RDR-159 refactor test

--- app/src/test/kotlin/me/alexandervortex/shelfie/data/mapper/ElementMapperTest.kt
+++ app/src/test/kotlin/me/alexandervortex/shelfie/data/mapper/ElementMapperTest.kt
@@ -1,7 +1,13 @@
 package me.alexandervortex.shelfie.data.mapper
 
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.data.forAll
+import io.kotest.data.headers
+import io.kotest.data.row
+import io.kotest.data.table
 import me.alexandervortex.shelfie.base.transformerTest
+import me.alexandervortex.shelfie.data.mapper.TestDataModels.emptyElementList
+import me.alexandervortex.shelfie.data.mapper.TestDataXml.emptyParagraph
 import me.alexandervortex.shelfie.data.mapper.TestDataXml.toBody
 
 class ElementMapperTest : BehaviorSpec({
@@ -9,9 +15,14 @@ class ElementMapperTest : BehaviorSpec({
     val mapper = ElementMapper()
     val binaries = TestDataXml.binaries()
 
-
-    transformerTest(TestDataModels.emptyElementList()) {
-        val body = TestDataXml.emptyParagraph().toBody()
-        mapper.map(body, binaries)
+    forAll(
+        table(
+            headers("xml", "model"),
+            row(emptyParagraph().toBody(), emptyElementList())
+        )
+    ) { xml, model ->
+        transformerTest(model) {
+            mapper.map(xml, binaries)
+        }
     }
 })
