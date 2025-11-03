7d29f2872c466fc1352b1cc961a7d1fa68552cb3
Alexander Vortex <avorteks@alfabank.ru>
2025-11-03 19:39:39 +0100

    RDR-159 new element ui structure

--- app/src/main/java/me/alexandervortex/shelfie/ui/model/ElementUI.kt
+++ app/src/main/java/me/alexandervortex/shelfie/ui/model/ElementUI.kt
@@ -3,12 +3,23 @@ package me.alexandervortex.shelfie.ui.model
 sealed interface ElementUI {
 
     data class TextUI(
-        val parts: List<String>,
+        // paragraph, quote, title, poem
+        val parts: List<StyledText>,
     ) : ElementUI
 
     data class ImageUI(
         val image: ByteArray,
     ) : ElementUI
 
-    data object EmptyLine : ElementUI
+    data object EmptyLineUI : ElementUI
+
+    data class DebugUI(
+        val type: String, // type of tag
+        val message: String, // error text
+    )
 }
+
+data class StyledText(
+    val style: TextStyle, // enum later
+    val text: String,
+)
