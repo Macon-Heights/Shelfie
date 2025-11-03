4359a91e94235d1f8cf2c7334d968e2acbb7bb78
Alexander Vortex <avorteks@alfabank.ru>
2025-11-03 21:12:24 +0100

    RDR-159 refactor of mapper

--- app/src/main/java/me/alexandervortex/shelfie/data/mapper/ElementMapper.kt
+++ app/src/main/java/me/alexandervortex/shelfie/data/mapper/ElementMapper.kt
@@ -1,8 +1,10 @@
 package me.alexandervortex.shelfie.data.mapper
 
-import me.alexandervortex.shelfie.base.ext.isPrimitive
+import me.alexandervortex.shelfie.base.ext.isNoChildren
 import me.alexandervortex.shelfie.base.ext.orEmpty
 import me.alexandervortex.shelfie.ui.model.ElementUI
+import me.alexandervortex.shelfie.ui.model.StyledText
+import me.alexandervortex.shelfie.ui.model.TextStyle
 import org.jsoup.nodes.Element
 import org.jsoup.nodes.TextNode
 import javax.inject.Inject
@@ -15,30 +17,38 @@ class ElementMapper @Inject constructor() {
     ): List<ElementUI> {
         if (element == null) return emptyList()
 
-        // Примитивные блоки (<image>, <empty-line>, <p>)
-        if (element.isPrimitiveTag()) {
-            return mapPrimitive(element, binaries).orEmpty()
+        // Блоки без вложенности
+        // Сюда же мы будем попадать рекурсивно, когда дойдем до конца ветки
+        if (element.isNoChildren()) {
+            return primitive(element, binaries).orEmpty()
         }
 
-        val children = element.childNodes().flatMap { node ->
-            when (node) {
-                is Element -> map(node, binaries)
-                is TextNode -> node.text().trim()
-                    .takeIf { it.isNotEmpty() }
-                    ?.let {
-                        listOf(
-                            ElementUI.TextUI(
-                                parts = it.split(SENTENCE_SEPARATOR)
-                                    .map { it.trim() }
-                                    .filter { it.isNotBlank() }
+        // допустим <p>
+        //
+        // Этотекст
+        // <strong>asd<strong>
+        // и еще текст
+        //
+        // <p>
+
+        val children = element.childNodes()
+            .flatMap { node ->
+                when (node) {
+                    is Element -> map(node, binaries) // прыгаем в рекурсию
+                    is TextNode -> node.text()
+                        .takeIf { it.isNotEmpty() }
+                        ?.let {
+                            listOf(
+                                ElementUI.TextUI(
+                                    parts = listOf(StyledText(TextStyle.Normal, it))
+                                )
                             )
-                        )
-                    }
-                    ?: emptyList()
+                        }
+                        ?: emptyList()
 
-                else -> emptyList()
+                    else -> emptyList()
+                }
             }
-        }
 
         return children
     }
@@ -49,11 +59,9 @@ class ElementMapper @Inject constructor() {
     ): ElementUI? {
         return when (element.tagName()) {
             "image" -> mapImage(element, binaries)
-            "empty-line" -> ElementUI.EmptyLine
-            "p", "v", "subtitle" -> ElementUI.TextUI(
-                element.text().trim().split(SENTENCE_SEPARATOR)
-                    .map { it.trim() }
-                    .filter { it.isNotBlank() }
+            "empty-line" -> ElementUI.EmptyLineUI
+            "p", "v", "subtitle" -> ElementUI.TextUI( // какие еще теги стоит обработать
+                listOf(StyledText(TextStyle.Normal, element.text()))
             )
 
             else -> null
