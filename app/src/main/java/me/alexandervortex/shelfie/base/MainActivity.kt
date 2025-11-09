b179cede8922bf1776c5966dc6bc7cc3d8852d6a
Alexander Vortex <avorteks@alfabank.ru>
2025-11-09 17:41:34 +0100

    RDR-159 add settings to activity

--- app/src/main/java/me/alexandervortex/shelfie/base/MainActivity.kt
+++ app/src/main/java/me/alexandervortex/shelfie/base/MainActivity.kt
@@ -10,20 +10,28 @@ import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
 import me.alexandervortex.shelfie.base.ext.getColors
 import me.alexandervortex.shelfie.features.navigate.RouterScreen
+import me.alexandervortex.shelfie.features.settings.AppSettingsRepository
+import me.alexandervortex.shelfie.features.settings.ProvideAppSettings
 import me.alexandervortex.shelfie.ui.theme.ShelfieTheme
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var appSettingsRepository: AppSettingsRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ShelfieTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = getColors().background
-                ) { RouterScreen() }
+            ProvideAppSettings(appSettingsRepository) {
+                ShelfieTheme {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = getColors().background
+                    ) { RouterScreen() }
+                }
             }
         }
     }
