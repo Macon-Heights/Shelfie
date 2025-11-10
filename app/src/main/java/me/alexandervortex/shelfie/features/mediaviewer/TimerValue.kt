d795ca2c4589b2f19d690847ebcac757caaed193
Alexander Vortex <avorteks@alfabank.ru>
2025-11-10 20:58:21 +0100

    RDR-159 add nice next and prev functions

--- app/src/main/java/me/alexandervortex/shelfie/features/mediaviewer/TimerValue.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/mediaviewer/TimerValue.kt
@@ -1,15 +1,28 @@
 package me.alexandervortex.shelfie.features.mediaviewer
 
-enum class TimerValue(val mins: Int, val text: String) {
+enum class TimerValue(
+    val value: Int,
+    val text: String,
+) : Switchable<TimerValue> {
 
-    OFF(0, " "),
+    OFF(0, "OFF"),
     TEST(1, "1m"),
     MIN_20(20, "20m"),
     MIN_40(40, "40m"),
     HOUR(60, "1h");
 
-    fun getNext(): TimerValue {
-        val nextIndex = (ordinal + 1) % entries.size
-        return entries[nextIndex]
-    }
+}
+
+interface Switchable<T : Enum<T>>
+
+inline fun <reified T> T.next(): T where T : Enum<T>, T : Switchable<T> {
+    val values = enumValues<T>()
+    val nextIndex = (ordinal + 1) % values.size
+    return values[nextIndex]
+}
+
+inline fun <reified T> T.prev(): T where T : Enum<T>, T : Switchable<T> {
+    val values = enumValues<T>()
+    val prevIndex = if (ordinal - 1 < 0) values.lastIndex else ordinal - 1
+    return values[prevIndex]
 }
