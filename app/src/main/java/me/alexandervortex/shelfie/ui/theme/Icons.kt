d2b42c283e01f7f8cf7ffb23f259b001a85cc137
Alexander Vortex <avorteks@alfabank.ru>
2025-11-12 19:09:57 +0100

    RDR-159 new icons added

--- app/src/main/java/me/alexandervortex/shelfie/ui/theme/Icons.kt
+++ app/src/main/java/me/alexandervortex/shelfie/ui/theme/Icons.kt
@@ -1,18 +1,19 @@
 package me.alexandervortex.shelfie.ui.theme
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Add
-import androidx.compose.material.icons.outlined.CheckCircle
-import androidx.compose.material.icons.outlined.ChevronLeft
-import androidx.compose.material.icons.outlined.ChevronRight
-import androidx.compose.material.icons.outlined.Circle
-import androidx.compose.material.icons.outlined.DeleteForever
-import androidx.compose.material.icons.outlined.Pause
-import androidx.compose.material.icons.outlined.PlayArrow
-import androidx.compose.material.icons.outlined.Speed
-import androidx.compose.material.icons.outlined.Timer
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.ChevronLeft
+import androidx.compose.material.icons.filled.ChevronRight
+import androidx.compose.material.icons.filled.Circle
+import androidx.compose.material.icons.filled.DeleteForever
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Speed
+import androidx.compose.material.icons.filled.Timer
 
-private val icons = Icons.Outlined
+private val icons = Icons.Default
 
 // Catalogue
 val IC_ADD = icons.Add
@@ -30,4 +31,5 @@ val IC_PLAYER_PREV = icons.ChevronLeft
 val IC_PLAYER_SPEED = icons.Speed
 val IC_PLAYER_TIMER = icons.Timer
 
-
+// Settings
+val IC_SETTINGS = icons.Settings
