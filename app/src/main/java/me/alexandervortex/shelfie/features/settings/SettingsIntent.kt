bfb3473638946dd06c6e7a39f3d22b345100df86
Alexander Vortex <avorteks@alfabank.ru>
2025-11-12 20:35:23 +0100

    RDR-159 add reset button

--- app/src/main/java/me/alexandervortex/shelfie/features/settings/SettingsIntent.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/settings/SettingsIntent.kt
@@ -1,7 +1,7 @@
 package me.alexandervortex.shelfie.features.settings
 
 sealed interface SettingsIntent {
-    data class ChangeFont(val value: Int) : SettingsIntent
+    data class ChangeFont(val value: Int = 16) : SettingsIntent
     data class ChangeTtsSpeed(val value: Float) : SettingsIntent
     data class ChangeStoppingTime(val value: Long) : SettingsIntent
 }
--- app/src/main/java/me/alexandervortex/shelfie/features/settings/SettingsItemComponent.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/settings/SettingsItemComponent.kt
@@ -3,15 +3,15 @@ package me.alexandervortex.shelfie.features.settings
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import me.alexandervortex.shelfie.base.ext.getColors
 import me.alexandervortex.shelfie.ui.component.refactored.ButtonComponent
-import me.alexandervortex.shelfie.ui.component.refactored.TITLE_SIZE
+import me.alexandervortex.shelfie.ui.theme.IC_RESET
 
 @Composable
 fun SettingsItemComponent(
@@ -19,34 +19,43 @@ fun SettingsItemComponent(
     value: String,
     onIncrease: () -> Unit,
     onDecrease: () -> Unit,
+    onReset: () -> Unit,
 ) {
     Row(verticalAlignment = Alignment.CenterVertically) {
         Text(
             modifier = Modifier.weight(1f),
             text = title,
-            fontSize = TITLE_SIZE.sp
         )
 
         ButtonComponent(
             containerColor = getColors().tertiary,
             contentColor = getColors().onTertiary,
-            modifier = Modifier.padding(horizontal = 8.dp),
+            modifier = Modifier.padding(horizontal = 4.dp),
             modifierAfter = Modifier.clickable {
                 onDecrease.invoke()
             },
             content = { Text(text = "-", color = it) },
         )
-
-        Text(
-            text = value,
-            fontSize = TITLE_SIZE.sp
-        )
+        Text(text = value)
         ButtonComponent(
-            modifier = Modifier.padding(horizontal = 8.dp),
+            modifier = Modifier.padding(start = 4.dp),
             modifierAfter = Modifier.clickable {
                 onIncrease.invoke()
             },
             content = { Text(text = "+", color = it) }
+        )
+        ButtonComponent(
+            modifier = Modifier.padding(start = 4.dp),
+            modifierAfter = Modifier.clickable {
+                onReset.invoke()
+            },
+            content = {
+                Icon(
+                    imageVector = IC_RESET,
+                    contentDescription = null,
+                    tint = it
+                )
+            }
         )
     }
 }
