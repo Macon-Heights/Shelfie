2b3b4c27617c68ee08cd7ffa7a7d36badf0055f2
Alexander Vortex <avorteks@alfabank.ru>
2025-11-14 02:01:48 +0100

    RDR-177 refactored theme value

--- app/src/main/java/me/alexandervortex/shelfie/features/settings/SettingsIntent.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/settings/SettingsIntent.kt
@@ -1,8 +1,13 @@
 package me.alexandervortex.shelfie.features.settings
 
+import me.alexandervortex.shelfie.features.settings.values.ThemeValue
+
 sealed interface SettingsIntent {
     data class ChangeFont(val value: Int = 16) : SettingsIntent
     data class ChangeLineHeight(val value: Float = 1f) : SettingsIntent
     data class ChangePadding(val value: Int = 24) : SettingsIntent
-    data class ChangeTheme(val value: Int = 0) : SettingsIntent
+
+    data class ChangeTheme(
+        val value: ThemeValue = ThemeValue.SYSTEM,
+    ) : SettingsIntent
 }
