d594c7061119f00a9b9caf038a5a1afaf9038fef
Alexander Vortex <avorteks@alfabank.ru>
2025-11-15 18:45:20 +0100

    RDR-180 start of mvi viewer refactoring

---
+++ app/src/main/java/me/alexandervortex/shelfie/features/viewer/mvi/ViewerState.kt
@@ -0,0 +1,12 @@
+package me.alexandervortex.shelfie.features.viewer.mvi
+
+import me.alexandervortex.shelfie.features.player.MediaServiceState
+import me.alexandervortex.shelfie.ui.model.BookUIModel
+
+data class ViewerState(
+    val book: BookUIModel? = null,
+    val serviceState: MediaServiceState = MediaServiceState(),
+    val isMenuVisible: Boolean = true,
+    val isSettingsVisible: Boolean = false,
+    val error: String = "",
+)
