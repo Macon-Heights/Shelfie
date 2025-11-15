bdce4d207f7dfcd9304156b657af25dbf346af5a
Alexander Vortex <avorteks@alfabank.ru>
2025-11-15 16:50:59 +0100

    RDR-180 little refactorin

--- app/src/main/java/me/alexandervortex/shelfie/features/mvi/catalogue/CatalogueViewModel.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/mvi/catalogue/CatalogueViewModel.kt
@@ -4,7 +4,6 @@ import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
