24b5c57e6d10a9ecf105a4f9a37d379db2f5057b
xandy <alex.vortex.dev@gmail.com>
2026-05-17 16:46:59 +0200

    finish mvp version off add book screen (need to make nice ui)

--- app/src/main/java/me/alexandervortex/shelfie/features/addbook/AddBookScreen.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/addbook/AddBookScreen.kt
@@ -1,10 +1,15 @@
 package me.alexandervortex.shelfie.features.addbook
 
 import android.net.Uri
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import me.alexandervortex.shelfie.features.addbook.mvi.AddBookEffect
+import me.alexandervortex.shelfie.features.addbook.mvi.AddBookIntent
 import me.alexandervortex.shelfie.ui.component.TitleUI
 
 @Composable
@@ -13,7 +18,21 @@ fun AddBookScreen(
     viewModel: AddBookViewModel,
     navController: NavHostController,
 ) {
+    val context = LocalContext.current
     val state by viewModel.state.collectAsStateWithLifecycle()
 
-    TitleUI("$uri")
+    LaunchedEffect(Unit) {
+        viewModel.onIntent(AddBookIntent.Add(uri))
+        viewModel.effect.collect { effect ->
+            when (effect) {
+                is AddBookEffect.ShowToast ->
+                    Toast.makeText(context, effect.message, Toast.LENGTH_LONG).show()
+
+                is AddBookEffect.NavigateTo ->
+                    navController.navigate(effect.route)
+            }
+        }
+    }
+
+    TitleUI(state.name)
 }
