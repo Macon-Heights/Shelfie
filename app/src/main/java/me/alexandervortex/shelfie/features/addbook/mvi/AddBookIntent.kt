7616612428eb07cda05dd5acef5b87dad7b2c328
xandy <alex.vortex.dev@gmail.com>
2026-05-17 16:45:30 +0200

    add intent and effect for new screen

--- app/src/main/java/me/alexandervortex/shelfie/features/addbook/mvi/AddBookIntent.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/addbook/mvi/AddBookIntent.kt
@@ -3,5 +3,5 @@ package me.alexandervortex.shelfie.features.addbook.mvi
 import android.net.Uri
 
 sealed interface AddBookIntent {
-    data class Add(val uri: Uri) : AddBookIntent
+    data class Add(val uri: Uri?) : AddBookIntent
 }
