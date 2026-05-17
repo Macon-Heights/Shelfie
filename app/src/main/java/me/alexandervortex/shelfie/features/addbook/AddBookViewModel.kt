5c3fc52f247544754932edd5c49e6ee03c6e02da
xandy <alex.vortex.dev@gmail.com>
2026-05-17 16:46:59 +0200

    finish mvp version off add book screen (need to make nice ui)

--- app/src/main/java/me/alexandervortex/shelfie/features/addbook/AddBookViewModel.kt
+++ app/src/main/java/me/alexandervortex/shelfie/features/addbook/AddBookViewModel.kt
@@ -2,14 +2,17 @@ package me.alexandervortex.shelfie.features.addbook
 
 import android.net.Uri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import me.alexandervortex.shelfie.features.addbook.mvi.AddBookEffect
 import me.alexandervortex.shelfie.features.addbook.mvi.AddBookIntent
 import me.alexandervortex.shelfie.features.addbook.mvi.AddBookState
-import me.alexandervortex.shelfie.features.mvi.catalogue.mvi.CatalogueEffect
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,16 +22,22 @@ class AddBookViewModel
     private val _state = MutableStateFlow(AddBookState("loading info"))
     val state = _state.asStateFlow()
 
-    private val _effect = MutableSharedFlow<CatalogueEffect>()
+    private val _effect = MutableSharedFlow<AddBookEffect>()
     val effect = _effect.asSharedFlow()
 
     fun onIntent(intent: AddBookIntent) {
         when (intent) {
-            is AddBookIntent.Add -> showPreview(intent.uri)
+            is AddBookIntent.Add -> loadPreview(intent.uri)
         }
     }
 
-    private fun showPreview(uri: Uri) {
-        // todo: add logic
+    private fun loadPreview(uri: Uri?) {
+        viewModelScope.launch {
+            uri?.let {
+                _state.update { state ->
+                    state.copy(name = uri.toString())
+                }
+            }
+        }
     }
 }
