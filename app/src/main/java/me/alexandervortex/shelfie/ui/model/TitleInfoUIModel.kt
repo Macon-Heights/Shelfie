1c205f4a02cde03611b5d618f1c2282a6c691e52
xandy <alex.vortex.dev@gmail.com>
2026-08-02 19:59:39 +0200

    fix bytearray in data class issue

--- app/src/main/java/me/alexandervortex/shelfie/ui/model/TitleInfoUIModel.kt
+++ app/src/main/java/me/alexandervortex/shelfie/ui/model/TitleInfoUIModel.kt
@@ -8,4 +8,32 @@ data class TitleInfoUIModel(
     val genre: String?,
     val lang: String?,
     val coverImage: ByteArray?,
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as TitleInfoUIModel
+
+        if (title != other.title) return false
+        if (date != other.date) return false
+        if (author != other.author) return false
+        if (annotation != other.annotation) return false
+        if (genre != other.genre) return false
+        if (lang != other.lang) return false
+        if (!coverImage.contentEquals(other.coverImage)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = title?.hashCode() ?: 0
+        result = 31 * result + (date?.hashCode() ?: 0)
+        result = 31 * result + (author?.hashCode() ?: 0)
+        result = 31 * result + (annotation?.hashCode() ?: 0)
+        result = 31 * result + (genre?.hashCode() ?: 0)
+        result = 31 * result + (lang?.hashCode() ?: 0)
+        result = 31 * result + (coverImage?.contentHashCode() ?: 0)
+        return result
+    }
+}
