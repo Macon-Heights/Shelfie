3918d6fc62b3118ac0ef9b14295f4578b6797c5d
xandy <alex.vortex.dev@gmail.com>
2026-08-21 10:56:54 +0200

    keep iterative refactoring

---
+++ app/src/main/java/me/alexandervortex/shelfie/ui/model/StyledText.kt
@@ -0,0 +1,53 @@
+package me.alexandervortex.shelfie.ui.model
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.text.style.TextDecoration
+
+data class StyledText(
+    val styles: Set<TextStyleUIModel>,
+    val text: String,
+)
+
+fun composeSpanStyle(
+    styles: Set<TextStyleUIModel>,
+    linkColor: Color,
+): SpanStyle {
+    var span = SpanStyle()
+
+    styles.forEach { style ->
+        when (style) {
+            TextStyleUIModel.Bold -> span = span.merge(SpanStyle(fontWeight = FontWeight.Black))
+            TextStyleUIModel.Italic -> span = span.merge(SpanStyle(fontStyle = FontStyle.Italic))
+            is TextStyleUIModel.Strikethrough -> span =
+                span.merge(SpanStyle(textDecoration = TextDecoration.LineThrough))
+
+            TextStyleUIModel.Underline -> span =
+                span.merge(SpanStyle(textDecoration = TextDecoration.Underline))
+
+            TextStyleUIModel.Sub -> span =
+                span.merge(SpanStyle(baselineShift = BaselineShift.Subscript))
+
+            TextStyleUIModel.Sup -> span =
+                span.merge(SpanStyle(baselineShift = BaselineShift.Superscript))
+
+            is TextStyleUIModel.Link -> span =
+                span.merge(SpanStyle(color = linkColor, textDecoration = TextDecoration.Underline))
+
+            TextStyleUIModel.Monospace -> span =
+                span.merge(SpanStyle(fontFamily = FontFamily.Monospace))
+
+            is TextStyleUIModel.Custom -> when (style.name) {
+                "strike" -> span =
+                    span.merge(SpanStyle(textDecoration = TextDecoration.LineThrough))
+
+                else -> {}
+            }
+        }
+    }
+    return span
+}
--- app/src/main/java/me/alexandervortex/shelfie/ui/model/UI.kt
+++ app/src/main/java/me/alexandervortex/shelfie/ui/model/UI.kt
@@ -1,17 +1,9 @@
 package me.alexandervortex.shelfie.ui.model
 
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.BaselineShift
-import androidx.compose.ui.text.style.TextDecoration
 import me.alexandervortex.shelfie.model.ImageModel
 
 sealed interface UI {
 
-    // basic
     data object EmptyLine : UI
 
     data class Image(
@@ -22,56 +14,10 @@ sealed interface UI {
         val parts: List<StyledText>,
     ) : UI
 
-    //
-
     data object Skeleton : UI
 
     data class Heading(
         val level: Int,
         val content: ComplexText,
     ) : UI
-}
-
-data class StyledText(
-    val styles: Set<TextStyleUIModel>,
-    val text: String,
-)
-
-fun composeSpanStyle(
-    styles: Set<TextStyleUIModel>,
-    linkColor: Color,
-): SpanStyle {
-    var span = SpanStyle()
-
-    styles.forEach { style ->
-        when (style) {
-            TextStyleUIModel.Bold -> span = span.merge(SpanStyle(fontWeight = FontWeight.Black))
-            TextStyleUIModel.Italic -> span = span.merge(SpanStyle(fontStyle = FontStyle.Italic))
-            is TextStyleUIModel.Strikethrough -> span =
-                span.merge(SpanStyle(textDecoration = TextDecoration.LineThrough))
-
-            TextStyleUIModel.Underline -> span =
-                span.merge(SpanStyle(textDecoration = TextDecoration.Underline))
-
-            TextStyleUIModel.Sub -> span =
-                span.merge(SpanStyle(baselineShift = BaselineShift.Subscript))
-
-            TextStyleUIModel.Sup -> span =
-                span.merge(SpanStyle(baselineShift = BaselineShift.Superscript))
-
-            is TextStyleUIModel.Link -> span =
-                span.merge(SpanStyle(color = linkColor, textDecoration = TextDecoration.Underline))
-
-            TextStyleUIModel.Monospace -> span =
-                span.merge(SpanStyle(fontFamily = FontFamily.Monospace))
-
-            is TextStyleUIModel.Custom -> when (style.name) {
-                "strike" -> span =
-                    span.merge(SpanStyle(textDecoration = TextDecoration.LineThrough))
-
-                else -> {}
-            }
-        }
-    }
-    return span
 }
