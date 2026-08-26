2c2c2ccd485b05da1dd461bf43062ede4ac7587d
xandy <alex.vortex.dev@gmail.com>
2026-08-26 20:48:50 +0200

    rearrange testdata

--- app/src/test/kotlin/me/alexandervortex/shelfie/data/mapper/ElementMapperTest.kt
+++ app/src/test/kotlin/me/alexandervortex/shelfie/data/mapper/ElementMapperTest.kt
@@ -6,19 +6,20 @@ import io.kotest.data.headers
 import io.kotest.data.row
 import io.kotest.data.table
 import me.alexandervortex.shelfie.base.transformerTest
-import me.alexandervortex.shelfie.data.mapper.ParagraphData.paragraphComplex
-import me.alexandervortex.shelfie.data.mapper.ParagraphData.paragraphComplexEpub
-import me.alexandervortex.shelfie.data.mapper.ParagraphData.paragraphComplexEpubModel
-import me.alexandervortex.shelfie.data.mapper.ParagraphData.paragraphComplexModel
-import me.alexandervortex.shelfie.data.mapper.ParagraphData.paragraphEmpty
-import me.alexandervortex.shelfie.data.mapper.ParagraphData.paragraphEmptyModel
-import me.alexandervortex.shelfie.data.mapper.ParagraphData.paragraphWithText
-import me.alexandervortex.shelfie.data.mapper.ParagraphData.paragraphWithTextModel
+import me.alexandervortex.shelfie.data.mapper.ElementMapperTestData.binaries
+import me.alexandervortex.shelfie.data.mapper.ElementMapperTestData.paragraphComplex
+import me.alexandervortex.shelfie.data.mapper.ElementMapperTestData.paragraphComplexEpub
+import me.alexandervortex.shelfie.data.mapper.ElementMapperTestData.paragraphComplexEpubModel
+import me.alexandervortex.shelfie.data.mapper.ElementMapperTestData.paragraphComplexModel
+import me.alexandervortex.shelfie.data.mapper.ElementMapperTestData.paragraphEmpty
+import me.alexandervortex.shelfie.data.mapper.ElementMapperTestData.paragraphEmptyModel
+import me.alexandervortex.shelfie.data.mapper.ElementMapperTestData.paragraphWithText
+import me.alexandervortex.shelfie.data.mapper.ElementMapperTestData.paragraphWithTextModel
 
 class ElementMapperTest : BehaviorSpec({
 
     val mapper = ElementMapper()
-    val binaries = TestDataXml.binaries()
+    val binaries = binaries()
 
     forAll(
         table(
--- app/src/test/kotlin/me/alexandervortex/shelfie/data/mapper/ElementMapperTestData.kt
+++ app/src/test/kotlin/me/alexandervortex/shelfie/data/mapper/ElementMapperTestData.kt
@@ -1,14 +1,27 @@
 package me.alexandervortex.shelfie.data.mapper
 
-import me.alexandervortex.shelfie.data.mapper.TestDataXml.toElement
 import me.alexandervortex.shelfie.model.BookDocument
 import me.alexandervortex.shelfie.model.BookNode
 import me.alexandervortex.shelfie.model.GroupKind
 import me.alexandervortex.shelfie.model.InlineNode
 import me.alexandervortex.shelfie.model.RichText
 import me.alexandervortex.shelfie.ui.model.TextStyleUIModel
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 
-object ParagraphData {
+object ElementMapperTestData {
+
+    fun String?.toElement(): Element? {
+        return this?.let { Jsoup.parse(it.trimIndent()) }
+    }
+
+    fun binaries(): Map<String, ByteArray> {
+        return mapOf(
+            "_1.jpg" to "_1.jpg".toByteArray(),
+            "_2.jpg" to "_2.jpg".toByteArray(),
+            "_3.jpg" to "_3.jpg".toByteArray()
+        )
+    }
 
     fun paragraphComplexEpub() = """
         <body class="z">
