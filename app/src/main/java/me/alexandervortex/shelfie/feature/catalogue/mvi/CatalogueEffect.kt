3c3c46a4d21f13d8294d46401a017622876072dc
xandy <alex.vortex.dev@gmail.com>
2026-09-08 22:07:05 +0200

    add intents and effects

--- app/src/main/java/me/alexandervortex/shelfie/feature/catalogue/mvi/CatalogueEffect.kt
+++ app/src/main/java/me/alexandervortex/shelfie/feature/catalogue/mvi/CatalogueEffect.kt
@@ -1,9 +1,6 @@
 package me.alexandervortex.shelfie.feature.catalogue.mvi
 
-import me.alexandervortex.shelfie.feature.updater.AppUpdate
-
 sealed interface CatalogueEffect {
     data class ShowToast(val message: String) : CatalogueEffect
     data class NavigateTo(val route: String) : CatalogueEffect
-    data class ShowUpdateDialog(val update: AppUpdate) : CatalogueEffect
 }
